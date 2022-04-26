431d2014798f9b00c8057eed5b7112671ebfb689
Roei Erez <roeierez@gmail.com>
2022-04-26 17:47:10 +0300

    append logger to console

--- lib/logger.dart
+++ lib/logger.dart
@@ -8,7 +8,6 @@ import 'package:path_provider/path_provider.dart';
 import 'package:share_extend/share_extend.dart';
 
 final Logger log = Logger('c-breez');
-
 Future<File> get _logFile async {
   var appDir = await getApplicationDocumentsDirectory();
   var logFile = File(appDir.path + "/log.txt");
@@ -29,18 +28,17 @@ Future shareFile(String filePath) {
 }
 
 class BreezLogger {
-  BreezLogger() {    
+  BreezLogger() {
+    log.onRecord.listen((record) {
+      debugPrint("${record.level}: ${record.message}");
+    });
     // Log flutter errors
-    FlutterError.onError = (FlutterErrorDetails details) {
-      if (details == null) {
-        print("Ignore log, details is null");
-        return;
-      }
+    FlutterError.onError = (FlutterErrorDetails details) {      
       FlutterError.presentError(details);
       final stack = details.stack?.toString() ?? "NoStack";
       final name = details.context?.name ?? "FlutterError";
-      final exception = details.exceptionAsString();      
-      print(exception + '\n' + stack + " --" + name);
+      final exception = details.exceptionAsString();
+      log.severe(exception + '\n' + stack + " --" + name);
     };
   }
 }

cdb9381cd957ae5862800067d1f569147c4da443
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-05-17 12:26:36 -0300

    Uses new model on wallet dashboard

--- lib/routes/home/status_text.dart
+++ lib/routes/home/status_text.dart
@@ -1,84 +1,84 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:c_breez/bloc/account/account_bloc.dart';
 import 'package:c_breez/bloc/account/account_state.dart';
+import 'package:c_breez/l10n/build_context_localizations.dart';
 import 'package:c_breez/utils/min_font_size.dart';
 import 'package:c_breez/widgets/loading_animated_text.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class StatusText extends StatefulWidget {
-  final AccountState account;
+class StatusText extends StatelessWidget {
   final String? message;
 
-  const StatusText(this.account, {this.message});
+  const StatusText({
+    Key? key,
+    this.message,
+  }) : super(key: key);
 
-  @override
-  State<StatefulWidget> createState() {
-    return _StatusTextState();
-  }
-}
-
-class _StatusTextState extends State<StatusText> {
   @override
   Widget build(BuildContext context) {
-    if (widget.message != null) {
-      return LoadingAnimatedText(widget.message!);
+    if (message != null) {
+      return LoadingAnimatedText(message!);
+    } else {
+      return BlocBuilder<AccountBloc, AccountState>(
+        builder: (context, account) => _build(context, account),
+      );
     }
+  }
 
-    if (widget.account.status == AccountStatus.CONNECTING) {
-      return LoadingAnimatedText("",
-          textAlign: TextAlign.center,
-          textElements: <TextSpan>[
-            TextSpan(
-              text: "Breez is ",
-              style: Theme.of(context).textTheme.bodyText2?.copyWith(
-                    color: Theme.of(context).colorScheme.onSecondary,
-                  ),
+  Widget _build(
+    BuildContext context,
+    AccountState account,
+  ) {
+    final themeData = Theme.of(context);
+    final texts = context.texts();
+
+    if (account.status == AccountStatus.CONNECTING) {
+      return LoadingAnimatedText(
+        "",
+        textAlign: TextAlign.center,
+        textElements: [
+          TextSpan(
+            text: texts.status_text_loading_begin,
+            style: themeData.textTheme.bodyText2?.copyWith(
+              color: themeData.colorScheme.onSecondary,
             ),
-            _LinkTextSpan(
-                text: "opening a secure channel",
-                // url: widget.account.channelFundingTxUrl,
-                style: Theme.of(context).textTheme.bodyText2!.copyWith(
-                    color: Theme.of(context).colorScheme.onSecondary,
-                    decoration: TextDecoration.underline)),
-            // style: theme.blueLinkStyle),
-            TextSpan(
-              text:
-                  " with our server. This might take a while, but don't worry, we'll notify you when the app is ready to send and receive payments.",
-              style: Theme.of(context).textTheme.bodyText2?.copyWith(
-                    color: Theme.of(context).colorScheme.onSecondary,
-                  ),
-            )
-          ]);
+          ),
+          TextSpan(
+            style: themeData.textTheme.bodyText2!.copyWith(
+              color: themeData.colorScheme.onSecondary,
+              decoration: TextDecoration.underline,
+            ),
+            text: texts.status_text_loading_middle,
+          ),
+          TextSpan(
+            text: texts.status_text_loading_end,
+            style: themeData.textTheme.bodyText2?.copyWith(
+              color: themeData.colorScheme.onSecondary,
+            ),
+          ),
+        ],
+      );
     }
 
-    if (widget.account.status == AccountStatus.CONNECTED) {
+    if (account.status == AccountStatus.CONNECTED) {
       return AutoSizeText(
-        "Breez is ready to receive funds.",
-        style: Theme.of(context).textTheme.bodyText2?.copyWith(
-              color: Theme.of(context).colorScheme.onSecondary,
-            ),
+        texts.status_text_ready,
+        style: themeData.textTheme.bodyText2?.copyWith(
+          color: themeData.colorScheme.onSecondary,
+        ),
         textAlign: TextAlign.center,
         minFontSize: MinFontSize(context).minFontSize,
         stepGranularity: 0.1,
       );
     }
 
-    return Text("Breez is opening a secure connection",
-        style: Theme.of(context).textTheme.bodyText2?.copyWith(
-              color: Theme.of(context).colorScheme.onSecondary,
-            ),
-        textAlign: TextAlign.center);
+    return Text(
+      texts.status_opening_secure_connection,
+      style: themeData.textTheme.bodyText2?.copyWith(
+        color: themeData.colorScheme.onSecondary,
+      ),
+      textAlign: TextAlign.center,
+    );
   }
-}
-
-class _LinkTextSpan extends TextSpan {
-  _LinkTextSpan({TextStyle? style, String? url, String? text})
-      : super(
-            style: style,
-            text: text ?? url,
-            recognizer: TapGestureRecognizer()
-              ..onTap = () {
-                launch(url!, forceSafariVC: false);
-              });
 }
--- lib/routes/home/wallet_dashboard_header_delegate.dart
+++ lib/routes/home/wallet_dashboard_header_delegate.dart
@@ -1,15 +1,8 @@
-import 'package:c_breez/bloc/account/account_bloc.dart';
-import 'package:c_breez/bloc/account/account_state.dart';
-import 'package:c_breez/bloc/currency/currency_bloc.dart';
-import 'package:c_breez/bloc/currency/currency_state.dart';
-import 'package:c_breez/bloc/user_profile/user_profile_bloc.dart';
-import 'package:c_breez/bloc/user_profile/user_profile_state.dart';
 import 'package:c_breez/routes/home/wallet_dashboard.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-const kDashboardMaxHeight = 202.25;
-const kDashboardMinHeight = 70.0;
+const kMaxExtent = 200.0;
+const kMinExtent = 70.0;
 
 class WalletDashboardHeaderDelegate extends SliverPersistentHeaderDelegate {
   const WalletDashboardHeaderDelegate();
@@ -20,45 +13,20 @@ class WalletDashboardHeaderDelegate extends SliverPersistentHeaderDelegate {
     double shrinkOffset,
     bool overlapsContent,
   ) {
-    return BlocBuilder<CurrencyBoc, CurrencyState>(
-      builder: (context, currencyState) {
-        return BlocBuilder<UserProfileBloc, UserProfileState>(
-          builder: (settingCtx, userState) {
-            return BlocBuilder<AccountBloc, AccountState>(
-              builder: (context, accountState) {
-                return Stack(
-                  clipBehavior: Clip.none,
-                  children: [
-                    WalletDashboard(
-                      userState.profileSettings,
-                      accountState,
-                      currencyState,
-                      (maxExtent - shrinkOffset).clamp(minExtent, maxExtent),
-                      (shrinkOffset / (maxExtent - minExtent)).clamp(0.0, 1.0),
-                      context.read<CurrencyBoc>().setBitcoinTicker,
-                      context.read<CurrencyBoc>().setFiatShortName,
-                      (hideBalance) => context
-                          .read<UserProfileBloc>()
-                          .updateProfile(hideBalance: true),
-                    ),
-                  ],
-                );
-              },
-            );
-          },
-        );
-      },
+    return WalletDashboard(
+      (kMaxExtent - shrinkOffset).clamp(kMinExtent, kMaxExtent),
+      (shrinkOffset / (kMaxExtent - kMinExtent)).clamp(0.0, 1.0),
     );
   }
 
   @override
-  double get maxExtent => kDashboardMaxHeight;
+  double get maxExtent => kMaxExtent;
 
   @override
-  double get minExtent => kDashboardMinHeight;
+  double get minExtent => kMinExtent;
 
   @override
   bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
-    return true;
+    return snapConfiguration != oldDelegate.snapConfiguration;
   }
 }

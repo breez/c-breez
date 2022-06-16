d182f7107cc4e16c2ecbac40ccd018a6ef2a8f45
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-06-16 19:11:03 +0300

    Check if input matches lnurl

--- packages/breez_sdk/lib/src/input_parser.dart
+++ packages/breez_sdk/lib/src/input_parser.dart
@@ -1,12 +1,14 @@
+import 'package:dart_lnurl/dart_lnurl.dart';
+
 import '../bridge_generated.dart';
 import 'native_toolkit.dart';
 
-
 class InputParser {
-  final LightningToolkit _lnToolkit = getNativeToolkit();  
+  final LightningToolkit _lnToolkit = getNativeToolkit();
+  static RegExp lnurlPrefix = RegExp(",*?((lnurl)([0-9]{1,}[a-z0-9]+){1})");
+  static RegExp lnurlRfc17Prefix = RegExp("(lnurl)(c|w|p)");
 
   Future<ParsedInput> parse(String s) async {
-
     // lightning link
     String lower = s.toLowerCase();
     if (lower.startsWith("lightning:")) {
@@ -15,9 +17,36 @@ class InputParser {
     }
 
     // lnurl
-    if (lower.startsWith("lnurl")) {
+    if (lower.contains(lnurlPrefix)) {
+      LNURLParseResult parseResult = await getParams(lower);
+      if (parseResult.payParams != null) {
+        return ParsedInput(InputProtocol.lnurlPay, lower);
+      }
+      if (parseResult.withdrawalParams != null) {
+        return ParsedInput(InputProtocol.lnurlWithdraw, lower);
+      }
       throw Exception("not implemented");
-    }    
+    } else if (lower.contains(lnurlRfc17Prefix) ||
+        lower.startsWith('keyauth://')) {
+      // Check if input is a valid url
+      if (Uri.tryParse(lower)?.hasAbsolutePath ?? false) {
+        // Replace prefix with https for clearnet URL's, http for onion URLs
+        var prefix =
+            RegExp(r'\.onion($|\W)').hasMatch(lower) ? 'http' : 'https';
+        final invoice = lower.replaceFirst(RegExp("(lnurl)(c|w|p)"), prefix);
+        InputProtocol? inputProtocol = lower.startsWith("lnurlp://")
+            ? InputProtocol.lnurlPay
+            : lower.startsWith("lnurlw://")
+                ? InputProtocol.lnurlWithdraw
+                : null;
+        if (inputProtocol != null) {
+          return ParsedInput(InputProtocol.lnurlPay, invoice);
+        }
+        throw Exception("not implemented");
+      }
+    } else if (lower.startsWith('https://')) {
+      throw Exception("not implemented");
+    }
 
     // bolt 11 lightning
     String? bolt11 = _extractBolt11FromBip21(lower);

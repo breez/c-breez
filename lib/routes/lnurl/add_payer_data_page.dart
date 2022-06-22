ede4ad94fd175a7d63418ff2caaa165498784f54
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-06-22 16:12:09 +0300

    Handle fixed amount invoices and re-use UI elements from CreateInvoicePage
    Use AmountFormField & ReceivableBTCBox for _amountController
    Add ReceivableBTCBox and labels,

--- lib/routes/lnurl/add_payer_data_page.dart
+++ lib/routes/lnurl/add_payer_data_page.dart
@@ -1,10 +1,18 @@
+import 'package:c_breez/bloc/account/account_bloc.dart';
+import 'package:c_breez/bloc/currency/currency_bloc.dart';
+import 'package:c_breez/bloc/lsp/lsp_bloc.dart';
 import 'package:c_breez/l10n/build_context_localizations.dart';
+import 'package:c_breez/utils/payment_validator.dart';
+import 'package:c_breez/widgets/amount_form_field/amount_form_field.dart';
 import 'package:c_breez/widgets/back_button.dart' as back_button;
+import 'package:c_breez/widgets/receivable_btc_box.dart';
 import 'package:c_breez/widgets/single_button_bottom_bar.dart';
 import 'package:dart_lnurl/dart_lnurl.dart';
 import 'package:email_validator/email_validator.dart';
+import 'package:fixnum/fixnum.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AddPayerDataPage extends StatefulWidget {
   final LNURLPayParams payParams;
@@ -31,10 +39,21 @@ class AddPayerDataPageState extends State<AddPayerDataPage> {
   final _k1Controller = TextEditingController();
   final _emailController = TextEditingController();
   final _identifierController = TextEditingController();
+  late final bool fixedAmount;
+
+  @override
+  void initState() {
+    super.initState();
+    fixedAmount = widget.payParams.minSendable == widget.payParams.maxSendable;
+    if (fixedAmount) {
+      _amountController.text = widget.payParams.maxSendable.toString();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final texts = context.texts();
+    final currencyState = context.read<CurrencyBloc>().state;
 
     return Scaffold(
       key: _scaffoldKey,
@@ -46,39 +65,40 @@ class AddPayerDataPageState extends State<AddPayerDataPage> {
       body: Form(
         key: _formKey,
         child: Padding(
-          padding: const EdgeInsets.only(top: 24.0, bottom: 40.0),
+          padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 40.0),
           child: Column(
             mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              TextFormField(
-                controller: _amountController,
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly
-                ],
-                validator: (value) {
-                  if (value != null) {
-                    try {
-                      var numValue = int.parse(value);
-                      if (numValue > widget.payParams.maxSendable &&
-                          numValue < widget.payParams.minSendable) {
-                        return "Enter an amount between ${widget.payParams.minSendable} and ${widget.payParams.maxSendable}.";
-                      }
-                    } catch (_) {
-                      return "Enter a valid amount.";
-                    }
-                  }
-                  return null;
-                },
-              ),
               if (widget.payParams.commentAllowed > 0) ...[
                 TextFormField(
                   controller: _commentController,
+                  keyboardType: TextInputType.multiline,
+                  textInputAction: TextInputAction.done,
+                  maxLines: null,
                   maxLength: widget.payParams.commentAllowed,
                   maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                  decoration: InputDecoration(
+                    labelText: texts.invoice_description_label,
+                  ),
                 )
               ],
+              AmountFormField(
+                enabled: !fixedAmount,
+                context: context,
+                texts: texts,
+                bitcoinCurrency: currencyState.bitcoinCurrency,
+                controller: _amountController,
+                validatorFn: validatePayment,
+              ),
+              ReceivableBTCBox(
+                receiveLabel: fixedAmount
+                    ? ''
+                    : '${texts.lnurl_fetch_invoice_limit(
+                        widget.payParams.minSendable.toString(),
+                        widget.payParams.maxSendable.toString(),
+                      )} sats.',
+              ),
               if (widget.payParams.payerData != null &&
                   widget.payParams.payerData!.name != null &&
                   widget.payParams.payerData!.name!.mandatory) ...[
@@ -120,49 +140,80 @@ class AddPayerDataPageState extends State<AddPayerDataPage> {
                   controller: _identifierController,
                 )
               ],
-              SingleButtonBottomBar(
-                text: "Add Payer Data",
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    final int amount = int.parse(_amountController.text);
-                    final String comment = _commentController.text;
-                    final PayerData payerData = PayerData(
-                      name: _nameController.text.isNotEmpty
-                          ? _nameController.text
-                          : null,
-                      pubkey: widget.payParams.payerData != null &&
-                              widget.payParams.payerData!.pubkey != null &&
-                              widget.payParams.payerData!.pubkey!.mandatory
-                          ? 'hex(<randomly generated secp256k1 pubkey>)'
-                          : null,
-                      auth: _k1Controller.text.isNotEmpty
-                          ? Auth(
-                              key: 'hex(<linkingKey>)',
-                              k1: _k1Controller.text,
-                              sig:
-                                  'hex(sign(hexToBytes(<${_k1Controller.text}>), <linkingPrivKey>))')
-                          : null,
-                      email: _emailController.text.isNotEmpty
-                          ? _emailController.text
-                          : null,
-                      identifier: _identifierController.text.isNotEmpty
-                          ? _identifierController.text
-                          : null,
-                    );
-                    var payerDataMap = {
-                      "amount": amount,
-                      "comment": comment,
-                      "payerData": payerData.toJson(),
-                    };
-                    widget.onSubmit(payerDataMap);
-                    Navigator.of(context).pop();
-                  }
-                },
-              )
             ],
           ),
         ),
       ),
+      bottomNavigationBar: SingleButtonBottomBar(
+        stickToBottom: true,
+        text: "PAY",
+        onPressed: () {
+          if (_formKey.currentState!.validate()) {
+            final int amount = int.parse(_amountController.text);
+            final String comment = _commentController.text;
+            final PayerData payerData = PayerData(
+              name:
+                  _nameController.text.isNotEmpty ? _nameController.text : null,
+              pubkey: widget.payParams.payerData != null &&
+                      widget.payParams.payerData!.pubkey != null &&
+                      widget.payParams.payerData!.pubkey!.mandatory
+                  ? 'hex(<randomly generated secp256k1 pubkey>)'
+                  : null,
+              auth: _k1Controller.text.isNotEmpty
+                  ? Auth(
+                      key: 'hex(<linkingKey>)',
+                      k1: _k1Controller.text,
+                      sig:
+                          'hex(sign(hexToBytes(<${_k1Controller.text}>), <linkingPrivKey>))')
+                  : null,
+              email: _emailController.text.isNotEmpty
+                  ? _emailController.text
+                  : null,
+              identifier: _identifierController.text.isNotEmpty
+                  ? _identifierController.text
+                  : null,
+            );
+            var payerDataMap = {
+              "amount": amount,
+              "comment": comment,
+              "payerData": payerData.toJson(),
+            };
+            widget.onSubmit(payerDataMap);
+            Navigator.of(context).pop();
+          }
+        },
+      ),
     );
+  }
+
+  String? validatePayment(Int64 amount) {
+    var accBloc = context.read<AccountBloc>();
+    late final accountState = accBloc.state;
+    late final lspStatus = context.read<LSPBloc>().state;
+    late final currencyState = context.read<CurrencyBloc>().state;
+    late final texts = context.texts();
+
+    if (amount > widget.payParams.maxSendable) {
+      return "Exceeds maximum sendable amount: ${widget.payParams.maxSendable}";
+    }
+    if (amount < widget.payParams.minSendable) {
+      return "Below minimum accepted amount: ${widget.payParams.minSendable}";
+    }
+
+    if (lspStatus.currentLSP != null) {
+      final channelMinimumFee = Int64(
+        lspStatus.currentLSP!.channelMinimumFeeMsat ~/ 1000,
+      );
+      if (amount > accountState.maxInboundLiquidity &&
+          amount <= channelMinimumFee) {
+        return texts.invoice_insufficient_amount_fee(
+          currencyState.bitcoinCurrency.format(channelMinimumFee),
+        );
+      }
+    }
+
+    return PaymentValidator(
+            accBloc.validatePayment, currencyState.bitcoinCurrency)
+        .validateIncoming(amount);
   }
 }

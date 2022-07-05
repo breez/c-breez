51ca9173ca7a516c22a25b381bc543372d573d36
Roei Erez <roeierez@gmail.com>
2022-07-05 09:51:01 +0300

    fix ios dependencies

--- packages/breez_sdk/ios/Classes/LightningToolkitPlugin.m
+++ packages/breez_sdk/ios/Classes/LightningToolkitPlugin.m
@@ -6,12 +6,12 @@
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "lightning_toolkit-Swift.h"
+#import "LightningToolkitPlugin.h"
 #endif
 
 @implementation LightningToolkitPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftLightningToolkitPlugin registerWithRegistrar:registrar];
+  [LightningToolkitPlugin registerWithRegistrar:registrar];
   dummy_method_to_enforce_bundling();
 }
 @end

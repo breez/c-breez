984e77c612b818796e631f37866ec0cc94888527
Ademar <ademar111190@gmail.com>
2022-08-15 18:28:36 -0300

    Add numpad widget (#93)

---
+++ lib/routes/lock_screen/widget/digit_button_widget.dart
@@ -0,0 +1,57 @@
+import 'package:c_breez/widgets/preview/preview.dart';
+import 'package:flutter/material.dart';
+
+class DigitButtonWidget extends StatelessWidget {
+  final String? digit;
+  final IconData? icon;
+  final Color? foregroundColor;
+  final Function(String?)? onPressed;
+
+  const DigitButtonWidget({
+    Key? key,
+    this.digit,
+    this.icon,
+    this.foregroundColor = Colors.white,
+    this.onPressed,
+  })  : assert(digit != null || icon != null, "Either digit or icon must be provided"),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final themeData = Theme.of(context);
+
+    return Center(
+      child: InkWell(
+        highlightColor: themeData.highlightColor,
+        customBorder: const CircleBorder(),
+        onTap: onPressed == null ? null : () => onPressed?.call(digit),
+        child: Center(
+          child: digit != null
+              ? Text(
+                  digit!,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: foregroundColor,
+                    fontSize: 20.0,
+                  ),
+                )
+              : Icon(
+                  icon,
+                  color: foregroundColor,
+                  size: 20.0,
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(Preview(List.generate(
+    10,
+    (index) => DigitButtonWidget(
+      digit: "$index",
+      onPressed: (digit) => print("Digit: $digit"),
+    ),
+  )));
+}

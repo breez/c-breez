0df370f052478d8b2ce9a29a4d68eefdb1f3f266
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-08-17 17:57:21 +0300

    Remove date filter slivers top Container
    This Container is likely added for testing purposes

--- lib/routes/home/widgets/payments_filter/header_filter_chip.dart
+++ lib/routes/home/widgets/payments_filter/header_filter_chip.dart
@@ -26,35 +26,32 @@ class HeaderFilterChip extends SliverPadding {
                   padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
                   height: maxHeight / 1.2,
                   color: theme.customData[theme.themeId]!.dashboardBgColor,
-                  child: Container(
-                    color: Colors.amber,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(5),
-                      child: Container(
-                        color:
-                            theme.customData[theme.themeId]!.paymentListBgColor,
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(16, 8, 0, 8),
-                              child: Chip(
-                                label: Text(
-                                  BreezDateUtils.formatFilterDateRange(
-                                    startDate,
-                                    endDate,
-                                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(5),
+                    child: Container(
+                      color:
+                          theme.customData[theme.themeId]!.paymentListBgColor,
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(16, 8, 0, 8),
+                            child: Chip(
+                              label: Text(
+                                BreezDateUtils.formatFilterDateRange(
+                                  startDate,
+                                  endDate,
                                 ),
-                                onDeleted: () => context
-                                    .read<AccountBloc>()
-                                    .changePaymentFilter(
-                                      breez_sdk.PaymentFilter(paymentTypes),
-                                    ),
                               ),
+                              onDeleted: () => context
+                                  .read<AccountBloc>()
+                                  .changePaymentFilter(
+                                    breez_sdk.PaymentFilter(paymentTypes),
+                                  ),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ),

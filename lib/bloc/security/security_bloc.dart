ea9559c9c9867c4dbfc24cc9e9f8ee6fe7fba9eb
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-08-30 12:38:46 -0300

    Add Create pin / Change pin page

--- lib/bloc/security/security_bloc.dart
+++ lib/bloc/security/security_bloc.dart
@@ -1,18 +1,23 @@
 import 'dart:async';
 
 import 'package:c_breez/bloc/security/security_state.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 class SecurityBloc extends Cubit<SecurityState> with HydratedMixin {
+  final _secureStorage = const FlutterSecureStorage();
+
   SecurityBloc() : super(const SecurityState.initial());
 
-  Future setPin() async {
+  Future setPin(String pin) async {
+    await _secureStorage.write(key: "pinCode", value: pin);
     emit(state.copyWith(
       pinStatus: PinStatus.enabled,
     ));
   }
 
   Future clearPin() async {
+    await _secureStorage.delete(key: "pinCode");
     emit(state.copyWith(
       pinStatus: PinStatus.disabled,
     ));

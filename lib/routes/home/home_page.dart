a21ad3f5b9e3a7ef9ebeeddb3ccd4f5188c00101
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-09-06 13:40:03 +0300

    Listen to network status changes and display no connection dialog if there is no connection

--- lib/routes/home/home_page.dart
+++ lib/routes/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:c_breez/handlers/check_version_handler.dart';
 import 'package:c_breez/handlers/input_handler.dart';
 import 'package:c_breez/routes/home/account_page.dart';
@@ -7,9 +9,12 @@ import 'package:c_breez/routes/home/widgets/close_popup.dart';
 import 'package:c_breez/routes/home/widgets/drawer/home_drawer.dart';
 import 'package:c_breez/routes/home/widgets/fade_in_widget.dart';
 import 'package:c_breez/routes/home/widgets/qr_action_button.dart';
+import 'package:c_breez/widgets/no_connection_dialog.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Home extends StatefulWidget {
@@ -29,6 +34,10 @@ class HomeState extends State<Home> {
   final GlobalKey firstPaymentItemKey = GlobalKey();
   final ScrollController scrollController = ScrollController();
 
+  ConnectivityResult _connectionStatus = ConnectivityResult.none;
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
   @override
   void initState() {
     super.initState();
@@ -40,7 +49,49 @@ class HomeState extends State<Home> {
         _scaffoldKey,
       );
       checkVersionDialog(context, context.read());
+      checkConnectivity();
+      _connectivitySubscription =
+          _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
     });
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> checkConnectivity() async {
+    late ConnectivityResult result;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      rethrow;
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    setState(() {
+      _connectionStatus = result;
+    });
+    if (_connectionStatus == ConnectivityResult.none) {
+      showNoConnectionDialog(context).then((retry) {
+        if (retry == true) {
+          Future.delayed(const Duration(seconds: 1), checkConnectivity);
+        }
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
   }
 
   @override

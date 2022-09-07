006ec412077bc3a06c0ded003f617081352decd6
Ademar Alves de Oliveira <ademar111190@gmail.com>
2022-09-07 12:20:20 -0300

    Launch lock screen at the startup if pin is enabled

--- lib/routes/security/lock_screen.dart
+++ lib/routes/security/lock_screen.dart
@@ -5,6 +5,8 @@ import 'package:c_breez/bloc/security/security_state.dart';
 import 'package:c_breez/l10n/build_context_localizations.dart';
 import 'package:c_breez/routes/security/widget/pin_code_widget.dart';
 import 'package:c_breez/theme/breez_light_theme.dart';
+import 'package:c_breez/widgets/route.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
@@ -12,57 +14,82 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
 class LockScreen extends StatelessWidget {
+  final AuthorizedAction authorizedAction;
+
   const LockScreen({
     Key? key,
+    required this.authorizedAction,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final texts = context.texts();
+    final navigator = Navigator.of(context);
 
-    return Scaffold(
-      body: BlocBuilder<SecurityBloc, SecurityState>(
-        builder: (context, state) {
-          return PinCodeWidget(
-            label: texts.lock_screen_enter_pin,
-            localAuthenticationOption: state.localAuthenticationOption,
-            testPinCodeFunction: (pin) async {
-              bool pinMatches = false;
-              try {
-                pinMatches = await context.read<SecurityBloc>().testPin(pin);
-              } catch (e) {
-                return TestPinResult(
-                  false,
-                  errorMessage: texts.lock_screen_pin_match_exception,
-                );
-              }
-              if (pinMatches) {
-                return const TestPinResult(true);
-              } else {
-                return TestPinResult(
-                  false,
-                  errorMessage: texts.lock_screen_pin_incorrect,
-                );
-              }
-            },
-            testBiometricsFunction: () async {
-              bool pinMatches = await context
-                  .read<SecurityBloc>()
-                  .localAuthentication(texts.security_and_backup_validate_biometrics_reason);
-              if (pinMatches) {
-                return const TestPinResult(true);
-              } else {
-                return TestPinResult(
-                  false,
-                  errorMessage: texts.lock_screen_pin_incorrect,
-                );
-              }
-            },
-          );
-        },
+    return WillPopScope(
+      onWillPop: () => SynchronousFuture(false),
+      child: Scaffold(
+        body: BlocBuilder<SecurityBloc, SecurityState>(
+          builder: (context, state) {
+            return PinCodeWidget(
+              label: texts.lock_screen_enter_pin,
+              localAuthenticationOption: state.localAuthenticationOption,
+              testPinCodeFunction: (pin) async {
+                bool pinMatches = false;
+                try {
+                  pinMatches = await context.read<SecurityBloc>().testPin(pin);
+                } catch (e) {
+                  return TestPinResult(
+                    false,
+                    errorMessage: texts.lock_screen_pin_match_exception,
+                  );
+                }
+                if (pinMatches) {
+                  _authorized(navigator);
+                  return const TestPinResult(true);
+                } else {
+                  return TestPinResult(
+                    false,
+                    errorMessage: texts.lock_screen_pin_incorrect,
+                  );
+                }
+              },
+              testBiometricsFunction: () async {
+                bool pinMatches = await context
+                    .read<SecurityBloc>()
+                    .localAuthentication(texts.security_and_backup_validate_biometrics_reason);
+                if (pinMatches) {
+                  _authorized(navigator);
+                  return const TestPinResult(true);
+                } else {
+                  return TestPinResult(
+                    false,
+                    errorMessage: texts.lock_screen_pin_incorrect,
+                  );
+                }
+              },
+            );
+          },
+        ),
       ),
     );
   }
+
+  void _authorized(NavigatorState navigator) {
+    switch (authorizedAction) {
+      case AuthorizedAction.launchHome:
+        navigator.pushReplacementNamed("/");
+        break;
+      case AuthorizedAction.popPage:
+        navigator.pop(true);
+        break;
+    }
+  }
+}
+
+enum AuthorizedAction {
+  launchHome,
+  popPage,
 }
 
 void main() async {
@@ -76,7 +103,20 @@ void main() async {
       ],
       child: MaterialApp(
         theme: breezLightTheme,
-        home: const LockScreen(),
+        home: LayoutBuilder(
+          builder: (context, _) => Center(
+            child: TextButton(
+              child: const Text("Launch lock screen"),
+              onPressed: () => Navigator.of(context).push(
+                FadeInRoute(
+                  builder: (_) => const LockScreen(
+                    authorizedAction: AuthorizedAction.popPage,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     )),
     storage: await HydratedStorage.build(

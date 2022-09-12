37e6f33f63eb2a7e6a090f472bdc123669f9ea56
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-09-12 13:37:24 +0300

    Use a more robust dismiss approach for dismissing flushbar
    popFlushbars() dismissed every flushbar on flusbarRoute which may have dismissed unwanted flusbars. This approach only dismissed the no connection flushbar if it's appearing or showing.

--- lib/handlers/connectivity_handler.dart
+++ lib/handlers/connectivity_handler.dart
@@ -1,7 +1,6 @@
 import 'package:another_flushbar/flushbar.dart';
 import 'package:c_breez/bloc/connectivity/connectivity_bloc.dart';
 import 'package:c_breez/theme/theme_provider.dart' as theme;
-import 'package:c_breez/widgets/flushbar.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,36 +8,47 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ConnectivityHandler {
   final BuildContext context;
+  late Flushbar flushbar;
 
   ConnectivityHandler(this.context) {
+    flushbar = _getNoConnectionFlushbar();
     final ConnectivityBloc connectivityBloc = context.read<ConnectivityBloc>();
     connectivityBloc.stream.listen((connectionStatus) {
       if (connectionStatus == ConnectivityResult.none) {
         showNoInternetConnectionFlushbar();
-      } else {
-        popFlushbars(context);
+      } else if (flushbar.isShowing() || flushbar.isAppearing()) {
+        flushbar.dismiss(true);
       }
     });
   }
 
   void showNoInternetConnectionFlushbar() {
+    if (flushbar.isShowing() || flushbar.isAppearing()) {
+      flushbar.dismiss(true);
+    }
+    flushbar.show(context);
+  }
+
+  Flushbar _getNoConnectionFlushbar() {
     final texts = AppLocalizations.of(context)!;
-    popFlushbars(context);
-    showFlushbar(
-      context,
-      position: FlushbarPosition.TOP,
+
+    Flushbar? flush;
+    flush = Flushbar(
+      isDismissible: false,
+      flushbarPosition: FlushbarPosition.TOP,
       icon: Icon(
         Icons.warning_amber_outlined,
         size: 28.0,
         color: Theme.of(context).errorColor,
       ),
-      messageWidget: Text(
+      messageText: Text(
         texts.no_connection_flushbar_title,
         style: theme.snackBarStyle,
         textAlign: TextAlign.center,
       ),
-      isDismissible: false,
-      duration: Duration.zero,
+      backgroundColor: theme.snackBarBackgroundColor,
     );
+
+    return flush;
   }
 }

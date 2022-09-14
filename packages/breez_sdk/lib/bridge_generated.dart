7ffceb5b6a62f84a1383c559c13697a28b36d280
Roei Erez <roeierez@gmail.com>
2022-09-14 14:15:37 +0300

    fix dart bridge generation

--- packages/breez_sdk/lib/bridge_generated.dart
+++ packages/breez_sdk/lib/bridge_generated.dart
@@ -879,5 +879,5 @@ class wire_list_route_hint extends ffi.Struct {
 }
 
 typedef DartPostCObjectFnType = ffi.Pointer<
-    ffi.NativeFunction<ffi.Uint8 Function(DartPort, ffi.Pointer<ffi.Void>)>>;
+    ffi.NativeFunction<ffi.Bool Function(DartPort, ffi.Pointer<ffi.Void>)>>;
 typedef DartPort = ffi.Int64;

ab3c0a194d9e43914fcd1b646937c3f7bf4c5924
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-11-07 17:46:36 +0300

    Refactor grpc::LspInformation to LspInformation conversion and make it's fields public

--- packages/breez_sdk/rust/src/lsp.rs
+++ packages/breez_sdk/rust/src/lsp.rs
@@ -9,52 +9,40 @@ use prost::Message;
 use tonic::Request;
 
 pub struct LspInformation {
-    id: Option<String>,
-    name: String,
-    widget_url: String,
-    pubkey: String,
-    host: String,
-    channel_capacity: i64,
-    target_conf: i32,
-    base_fee_msat: i64,
-    fee_rate: f64,
-    time_lock_delate: u32,
-    min_htlc_msat: i64,
-    channel_fee_permyriad: i64,
-    lsp_pubkey: Vec<u8>,
-    max_inactive_duration: i64,
-    channel_minimum_fee_msat: i64,
+    pub id: String,
+    pub name: String,
+    pub widget_url: String,
+    pub pubkey: String,
+    pub host: String,
+    pub channel_capacity: i64,
+    pub target_conf: i32,
+    pub base_fee_msat: i64,
+    pub fee_rate: f64,
+    pub time_lock_delta: u32,
+    pub min_htlc_msat: i64,
+    pub channel_fee_permyriad: i64,
+    pub lsp_pubkey: Vec<u8>,
+    pub max_inactive_duration: i64,
+    pub channel_minimum_fee_msat: i64,
 }
 
-impl From<grpc::LspInformation> for LspInformation {
-    fn from(lsp_info: grpc::LspInformation) -> Self {
-        LspInformation {
-            id: None,
-            name: lsp_info.name,
-            widget_url: lsp_info.widget_url,
-            pubkey: lsp_info.pubkey,
-            host: lsp_info.host,
-            channel_capacity: lsp_info.channel_capacity,
-            target_conf: lsp_info.target_conf,
-            base_fee_msat: lsp_info.base_fee_msat,
-            fee_rate: lsp_info.fee_rate,
-            time_lock_delate: lsp_info.time_lock_delta,
-            min_htlc_msat: lsp_info.min_htlc_msat,
-            channel_fee_permyriad: lsp_info.channel_fee_permyriad,
-            lsp_pubkey: lsp_info.lsp_pubkey,
-            max_inactive_duration: lsp_info.max_inactive_duration,
-            channel_minimum_fee_msat: lsp_info.channel_minimum_fee_msat,
-        }
-    }
-}
-trait AddID {
-    fn add_id(lsp_id: String, lsp_info: LspInformation) -> LspInformation;
-}
-
-impl AddID for LspInformation {
-    fn add_id(lsp_id: String, mut lsp_info: LspInformation) -> LspInformation {
-        lsp_info.id = Some(lsp_id);
-        lsp_info
+fn convert_to_lsp_info(lsp_id: String, lsp_info: grpc::LspInformation) -> LspInformation {
+    LspInformation {
+        id: lsp_id,
+        name: lsp_info.name,
+        widget_url: lsp_info.widget_url,
+        pubkey: lsp_info.pubkey,
+        host: lsp_info.host,
+        channel_capacity: lsp_info.channel_capacity,
+        target_conf: lsp_info.target_conf,
+        base_fee_msat: lsp_info.base_fee_msat,
+        fee_rate: lsp_info.fee_rate,
+        time_lock_delta: lsp_info.time_lock_delta,
+        min_htlc_msat: lsp_info.min_htlc_msat,
+        channel_fee_permyriad: lsp_info.channel_fee_permyriad,
+        lsp_pubkey: lsp_info.lsp_pubkey,
+        max_inactive_duration: lsp_info.max_inactive_duration,
+        channel_minimum_fee_msat: lsp_info.channel_minimum_fee_msat,
     }
 }
 
@@ -67,7 +55,7 @@ impl LspAPI for BreezServer {
         let response = client.lsp_list(request).await?;
         let mut lsp_list: Vec<LspInformation> = Vec::new();
         for (key, value) in response.into_inner().lsps.into_iter() {
-            lsp_list.push(LspInformation::add_id(key, LspInformation::from(value)));
+            lsp_list.push(convert_to_lsp_info(key, value));
         }
         Ok(lsp_list)
     }

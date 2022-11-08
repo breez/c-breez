7855a89d8946e39a041f490a94cba45fb8e99565
Roei Erez <roeierez@gmail.com>
2022-11-08 12:05:09 +0200

    generate dart classes and move to branch sign_invoice

--- packages/breez_sdk/ios/Classes/bridge_generated.h
+++ packages/breez_sdk/ios/Classes/bridge_generated.h
@@ -2,34 +2,19 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+typedef int64_t DartPort;
+
+typedef bool (*DartPostCObjectFnType)(DartPort port_id, void *message);
+
 typedef struct wire_uint_8_list {
   uint8_t *ptr;
   int32_t len;
 } wire_uint_8_list;
 
-typedef struct wire_RouteHintHop {
-  struct wire_uint_8_list *src_node_id;
-  uint64_t short_channel_id;
-  uint32_t fees_base_msat;
-  uint32_t fees_proportional_millionths;
-  uint64_t cltv_expiry_delta;
-  uint64_t *htlc_minimum_msat;
-  uint64_t *htlc_maximum_msat;
-} wire_RouteHintHop;
-
-typedef struct wire_list_route_hint_hop {
-  struct wire_RouteHintHop *ptr;
-  int32_t len;
-} wire_list_route_hint_hop;
-
-typedef struct wire_RouteHint {
-  struct wire_list_route_hint_hop *field0;
-} wire_RouteHint;
-
-typedef struct wire_list_route_hint {
-  struct wire_RouteHint *ptr;
-  int32_t len;
-} wire_list_route_hint;
+typedef struct wire_GreenlightCredentials {
+  struct wire_uint_8_list *device_key;
+  struct wire_uint_8_list *device_cert;
+} wire_GreenlightCredentials;
 
 typedef struct WireSyncReturnStruct {
   uint8_t *ptr;
@@ -37,81 +22,70 @@ typedef struct WireSyncReturnStruct {
   bool success;
 } WireSyncReturnStruct;
 
-typedef int64_t DartPort;
+void store_dart_post_cobject(DartPostCObjectFnType ptr);
 
-typedef bool (*DartPostCObjectFnType)(DartPort port_id, void *message);
+void wire_register_node(int64_t port_, int32_t network, struct wire_uint_8_list *seed);
 
-void wire_init_hsmd(int64_t port_,
-                    struct wire_uint_8_list *storage_path,
-                    struct wire_uint_8_list *secret);
+void wire_recover_node(int64_t port_, int32_t network, struct wire_uint_8_list *seed);
 
-void wire_mnemonic_to_seed(int64_t port_, struct wire_uint_8_list *phrase);
+void wire_create_node_services(int64_t port_,
+                               int32_t network,
+                               struct wire_uint_8_list *seed,
+                               struct wire_GreenlightCredentials *creds);
 
-void wire_create_swap(int64_t port_);
+void wire_start_node(int64_t port_);
 
-void wire_create_submaring_swap_script(int64_t port_,
-                                       struct wire_uint_8_list *hash,
-                                       struct wire_uint_8_list *swapper_pub_key,
-                                       struct wire_uint_8_list *payer_pub_key,
-                                       int64_t lock_height);
+void wire_run_signer(int64_t port_);
 
-void wire_encrypt(int64_t port_, struct wire_uint_8_list *key, struct wire_uint_8_list *msg);
+void wire_stop_signer(int64_t port_);
 
-void wire_decrypt(int64_t port_, struct wire_uint_8_list *key, struct wire_uint_8_list *msg);
+void wire_sync(int64_t port_);
 
-void wire_parse_invoice(int64_t port_, struct wire_uint_8_list *invoice);
+void wire_list_lsps(int64_t port_);
 
-void wire_node_pubkey(int64_t port_,
-                      struct wire_uint_8_list *storage_path,
-                      struct wire_uint_8_list *secret);
+void wire_set_lsp_id(int64_t port_, struct wire_uint_8_list *lsp_id);
 
-void wire_add_routing_hints(int64_t port_,
-                            struct wire_uint_8_list *storage_path,
-                            struct wire_uint_8_list *secret,
-                            struct wire_uint_8_list *invoice,
-                            struct wire_list_route_hint *hints,
-                            uint64_t new_amount);
+void wire_get_node_state(int64_t port_);
 
-void wire_sign_message(int64_t port_,
-                       struct wire_uint_8_list *storage_path,
-                       struct wire_uint_8_list *secret,
-                       struct wire_uint_8_list *msg);
+void wire_fetch_rates(int64_t port_);
 
-void wire_handle(int64_t port_,
-                 struct wire_uint_8_list *storage_path,
-                 struct wire_uint_8_list *secret,
-                 struct wire_uint_8_list *msg,
-                 struct wire_uint_8_list *peer_id,
-                 uint64_t db_id);
+void wire_list_fiat_currencies(int64_t port_);
 
-uint64_t *new_box_autoadd_u64_0(uint64_t value);
+void wire_list_transactions(int64_t port_,
+                            int32_t filter,
+                            int64_t *from_timestamp,
+                            int64_t *to_timestamp);
 
-struct wire_list_route_hint *new_list_route_hint_0(int32_t len);
+void wire_request_payment(int64_t port_,
+                          uint64_t amount_sats,
+                          struct wire_uint_8_list *description);
 
-struct wire_list_route_hint_hop *new_list_route_hint_hop_0(int32_t len);
+struct wire_GreenlightCredentials *new_box_autoadd_greenlight_credentials_0(void);
+
+int64_t *new_box_autoadd_i64_0(int64_t value);
 
 struct wire_uint_8_list *new_uint_8_list_0(int32_t len);
 
 void free_WireSyncReturnStruct(struct WireSyncReturnStruct val);
 
-void store_dart_post_cobject(DartPostCObjectFnType ptr);
-
 static int64_t dummy_method_to_enforce_bundling(void) {
     int64_t dummy_var = 0;
-    dummy_var ^= ((int64_t) (void*) wire_init_hsmd);
-    dummy_var ^= ((int64_t) (void*) wire_mnemonic_to_seed);
-    dummy_var ^= ((int64_t) (void*) wire_create_swap);
-    dummy_var ^= ((int64_t) (void*) wire_create_submaring_swap_script);
-    dummy_var ^= ((int64_t) (void*) wire_encrypt);
-    dummy_var ^= ((int64_t) (void*) wire_decrypt);
-    dummy_var ^= ((int64_t) (void*) wire_parse_invoice);
-    dummy_var ^= ((int64_t) (void*) wire_node_pubkey);
-    dummy_var ^= ((int64_t) (void*) wire_add_routing_hints);
-    dummy_var ^= ((int64_t) (void*) wire_sign_message);
-    dummy_var ^= ((int64_t) (void*) wire_handle);
-    dummy_var ^= ((int64_t) (void*) new_box_autoadd_u64_0);
-    dummy_var ^= ((int64_t) (void*) new_list_route_hint_0);
-    dummy_var ^= ((int64_t) (void*) new_list_route_hint_hop_0);
+    dummy_var ^= ((int64_t) (void*) wire_register_node);
+    dummy_var ^= ((int64_t) (void*) wire_recover_node);
+    dummy_var ^= ((int64_t) (void*) wire_create_node_services);
+    dummy_var ^= ((int64_t) (void*) wire_start_node);
+    dummy_var ^= ((int64_t) (void*) wire_run_signer);
+    dummy_var ^= ((int64_t) (void*) wire_stop_signer);
+    dummy_var ^= ((int64_t) (void*) wire_sync);
+    dummy_var ^= ((int64_t) (void*) wire_list_lsps);
+    dummy_var ^= ((int64_t) (void*) wire_set_lsp_id);
+    dummy_var ^= ((int64_t) (void*) wire_get_node_state);
+    dummy_var ^= ((int64_t) (void*) wire_fetch_rates);
+    dummy_var ^= ((int64_t) (void*) wire_list_fiat_currencies);
+    dummy_var ^= ((int64_t) (void*) wire_list_transactions);
+    dummy_var ^= ((int64_t) (void*) wire_request_payment);
+    dummy_var ^= ((int64_t) (void*) new_box_autoadd_greenlight_credentials_0);
+    dummy_var ^= ((int64_t) (void*) new_box_autoadd_i64_0);
     dummy_var ^= ((int64_t) (void*) new_uint_8_list_0);
     dummy_var ^= ((int64_t) (void*) free_WireSyncReturnStruct);
     dummy_var ^= ((int64_t) (void*) store_dart_post_cobject);
---
+++ packages/breez_sdk/rust/src/bridge_generated.io.rs
@@ -0,0 +1,185 @@
+use super::*;
+// Section: wire functions
+
+#[no_mangle]
+pub extern "C" fn wire_register_node(port_: i64, network: i32, seed: *mut wire_uint_8_list) {
+    wire_register_node_impl(port_, network, seed)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_recover_node(port_: i64, network: i32, seed: *mut wire_uint_8_list) {
+    wire_recover_node_impl(port_, network, seed)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_create_node_services(
+    port_: i64,
+    network: i32,
+    seed: *mut wire_uint_8_list,
+    creds: *mut wire_GreenlightCredentials,
+) {
+    wire_create_node_services_impl(port_, network, seed, creds)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_start_node(port_: i64) {
+    wire_start_node_impl(port_)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_run_signer(port_: i64) {
+    wire_run_signer_impl(port_)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_stop_signer(port_: i64) {
+    wire_stop_signer_impl(port_)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_sync(port_: i64) {
+    wire_sync_impl(port_)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_list_lsps(port_: i64) {
+    wire_list_lsps_impl(port_)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_set_lsp_id(port_: i64, lsp_id: *mut wire_uint_8_list) {
+    wire_set_lsp_id_impl(port_, lsp_id)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_get_node_state(port_: i64) {
+    wire_get_node_state_impl(port_)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_fetch_rates(port_: i64) {
+    wire_fetch_rates_impl(port_)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_list_fiat_currencies(port_: i64) {
+    wire_list_fiat_currencies_impl(port_)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_list_transactions(
+    port_: i64,
+    filter: i32,
+    from_timestamp: *mut i64,
+    to_timestamp: *mut i64,
+) {
+    wire_list_transactions_impl(port_, filter, from_timestamp, to_timestamp)
+}
+
+#[no_mangle]
+pub extern "C" fn wire_request_payment(
+    port_: i64,
+    amount_sats: u64,
+    description: *mut wire_uint_8_list,
+) {
+    wire_request_payment_impl(port_, amount_sats, description)
+}
+
+// Section: allocate functions
+
+#[no_mangle]
+pub extern "C" fn new_box_autoadd_greenlight_credentials_0() -> *mut wire_GreenlightCredentials {
+    support::new_leak_box_ptr(wire_GreenlightCredentials::new_with_null_ptr())
+}
+
+#[no_mangle]
+pub extern "C" fn new_box_autoadd_i64_0(value: i64) -> *mut i64 {
+    support::new_leak_box_ptr(value)
+}
+
+#[no_mangle]
+pub extern "C" fn new_uint_8_list_0(len: i32) -> *mut wire_uint_8_list {
+    let ans = wire_uint_8_list {
+        ptr: support::new_leak_vec_ptr(Default::default(), len),
+        len,
+    };
+    support::new_leak_box_ptr(ans)
+}
+
+// Section: impl Wire2Api
+
+impl Wire2Api<String> for *mut wire_uint_8_list {
+    fn wire2api(self) -> String {
+        let vec: Vec<u8> = self.wire2api();
+        String::from_utf8_lossy(&vec).into_owned()
+    }
+}
+impl Wire2Api<GreenlightCredentials> for *mut wire_GreenlightCredentials {
+    fn wire2api(self) -> GreenlightCredentials {
+        let wrap = unsafe { support::box_from_leak_ptr(self) };
+        Wire2Api::<GreenlightCredentials>::wire2api(*wrap).into()
+    }
+}
+
+impl Wire2Api<GreenlightCredentials> for wire_GreenlightCredentials {
+    fn wire2api(self) -> GreenlightCredentials {
+        GreenlightCredentials {
+            device_key: self.device_key.wire2api(),
+            device_cert: self.device_cert.wire2api(),
+        }
+    }
+}
+
+impl Wire2Api<Vec<u8>> for *mut wire_uint_8_list {
+    fn wire2api(self) -> Vec<u8> {
+        unsafe {
+            let wrap = support::box_from_leak_ptr(self);
+            support::vec_from_leak_ptr(wrap.ptr, wrap.len)
+        }
+    }
+}
+// Section: wire structs
+
+#[repr(C)]
+#[derive(Clone)]
+pub struct wire_GreenlightCredentials {
+    device_key: *mut wire_uint_8_list,
+    device_cert: *mut wire_uint_8_list,
+}
+
+#[repr(C)]
+#[derive(Clone)]
+pub struct wire_uint_8_list {
+    ptr: *mut u8,
+    len: i32,
+}
+
+// Section: impl NewWithNullPtr
+
+pub trait NewWithNullPtr {
+    fn new_with_null_ptr() -> Self;
+}
+
+impl<T> NewWithNullPtr for *mut T {
+    fn new_with_null_ptr() -> Self {
+        std::ptr::null_mut()
+    }
+}
+
+impl NewWithNullPtr for wire_GreenlightCredentials {
+    fn new_with_null_ptr() -> Self {
+        Self {
+            device_key: core::ptr::null_mut(),
+            device_cert: core::ptr::null_mut(),
+        }
+    }
+}
+
+// Section: sync execution mode utility
+
+#[no_mangle]
+pub extern "C" fn free_WireSyncReturnStruct(val: support::WireSyncReturnStruct) {
+    unsafe {
+        let _ = support::vec_from_leak_ptr(val.ptr, val.len);
+    }
+}

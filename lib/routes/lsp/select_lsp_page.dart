4c507a647e2b77d8dd30ea9e5cf5db8d487a25bd
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-11-29 01:41:16 +0300

    Use widgets instead of helper methods on Select Lsp Page

--- lib/routes/lsp/select_lsp_page.dart
+++ lib/routes/lsp/select_lsp_page.dart
@@ -1,9 +1,9 @@
 import 'package:breez_sdk/bridge_generated.dart';
 import 'package:c_breez/bloc/lsp/lsp_bloc.dart';
 import 'package:c_breez/l10n/build_context_localizations.dart';
+import 'package:c_breez/routes/lsp/widgets/lsp_list_widget.dart';
 import 'package:c_breez/widgets/loader.dart';
 import 'package:c_breez/widgets/single_button_bottom_bar.dart';
-import 'package:fimber/fimber.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -19,156 +19,92 @@ class SelectLSPPage extends StatefulWidget {
 }
 
 class SelectLSPPageState extends State<SelectLSPPage> {
-  LspInformation? _selectedLSP;
-  final _log = FimberLog("LspPage");
+  LspInformation? _selectedLsp;
+  Object? _error;
 
   @override
   Widget build(BuildContext context) {
-    final lspBloc = context.read<LSPBloc>();
     final texts = context.texts();
 
-    return StreamBuilder<LspInformation?>(
-      stream: lspBloc.stream,
-      builder: (context, lspStreamSnapshot) {
-        return FutureBuilder(
-          future: lspBloc.lspList,
-          builder: (context, lspListSnapshot) {
-            return FutureBuilder(
-              future: lspBloc.currentLSP,
-              builder: (context, currentLspSnapshot) {
-                final error = currentLspSnapshot.error ?? lspStreamSnapshot.error ?? lspListSnapshot.error;
-
-                return Scaffold(
-                  appBar: AppBar(
-                    automaticallyImplyLeading: false,
-                    title: Text(texts.account_page_activation_provider_label),
-                    actions: [
-                      IconButton(
-                        onPressed: () => Navigator.pop(context),
-                        // Color needs to be changed
-                        icon: Icon(
-                          Icons.close,
-                          color: Theme.of(context).appBarTheme.iconTheme?.color,
-                        ),
-                      )
-                    ],
-                  ),
-                  body: lspListSnapshot.hasData
-                      ? error != null
-                          ? _error(context, error)
-                          : _body(context, currentLspSnapshot.data, lspListSnapshot.data!)
-                      : const Center(child: Loader()),
-                  bottomNavigationBar: lspListSnapshot.hasData
-                      ? _buildBottomButton(context, _selectedLSP ?? currentLspSnapshot.data, error)
-                      : null,
-                );
-              },
-            );
-          },
-        );
-      },
-    );
-  }
-
-  Widget _error(BuildContext context, Object error) {
-    _log.i("Error: $error");
-    final texts = context.texts();
-
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(
-            texts.account_page_activation_error,
-            textAlign: TextAlign.center,
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(texts.account_page_activation_provider_label),
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.pop(context),
+            // Color needs to be changed
+            icon: Icon(
+              Icons.close,
+              color: Theme.of(context).appBarTheme.iconTheme?.color,
+            ),
+          )
         ],
       ),
-    );
-  }
-
-  Widget _body(
-    BuildContext context,
-    LspInformation? currentLsp,
-    List<LspInformation> lspList,
-  ) {
-    final texts = context.texts();
-    final selectedLSP = _selectedLSP ?? currentLsp;
-
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Flexible(
-            child: Text(
-              texts.account_page_activation_provider_hint,
-              style: const TextStyle(color: Colors.white),
-              textAlign: TextAlign.left,
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.all(48.0),
-              child: ListView.builder(
-                shrinkWrap: false,
-                itemCount: lspList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 0.0),
-                    selected: selectedLSP?.id == lspList[index].id,
-                    trailing: selectedLSP?.id == lspList[index].id ? const Icon(Icons.check) : null,
-                    title: Text(lspList[index].name),
-                    onTap: () {
-                      setState(() {
-                        _selectedLSP = lspList[index];
-                      });
-                    },
-                  );
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget? _buildBottomButton(
-    BuildContext context,
-    LspInformation? selectedLSP,
-    Object? error,
-  ) {
-    final texts = context.texts();
-
-    if (error != null) {
-      return SingleButtonBottomBar(
-        text: texts.account_page_activation_action_retry,
-        onPressed: () {
+      body: LspListWidget(
+        selectedLsp: _selectedLsp,
+        onError: (error) {
           setState(() {
-            _selectedLSP = null;
+            _error = error;
           });
         },
-      );
+        onSelected: (selectedLsp) {
+          setState(() {
+            _selectedLsp = selectedLsp;
+          });
+        },
+      ),
+      bottomNavigationBar: _LspActionButton(
+        selectedLsp: _selectedLsp,
+        error: _error,
+        resetLsp: () {
+          setState(() {
+            _selectedLsp = null;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class _LspActionButton extends StatelessWidget {
+  final LspInformation? selectedLsp;
+  final Object? error;
+  final Function resetLsp;
+
+  const _LspActionButton(
+      {Key? key, this.selectedLsp, required this.error, required this.resetLsp})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final texts = context.texts();
+    late final String message;
+    late final Function() onPressed;
+
+    if (error == null && selectedLsp == null) {
+      return const SizedBox();
     }
 
-    if (selectedLSP != null) {
-      return SingleButtonBottomBar(
-        text: texts.account_page_activation_action_select,
-        onPressed: () async {
-          await _connectToLSP(selectedLSP);
-        },
-      );
+    if (error != null) {
+      message = texts.account_page_activation_action_retry;
+      onPressed = resetLsp();
     }
-    return null;
+    if (selectedLsp != null) {
+      message = texts.account_page_activation_action_select;
+      onPressed = () async {
+        await _connectToLSP(selectedLsp!, context);
+      };
+    }
+
+    return SingleButtonBottomBar(
+      text: message,
+      onPressed: onPressed,
+    );
   }
 
-  Future<void> _connectToLSP(LspInformation lspInformation) async {
+  Future<void> _connectToLSP(
+      LspInformation lspInformation, BuildContext context) async {
     final navigator = Navigator.of(context);
     var loaderRoute = createLoaderRoute(context);
     navigator.push(loaderRoute);

b386de9fd70cf83ac56b3f94327655813ad5dfa5
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-12-07 16:07:12 +0300

    Updated dependencies
    - cargo install flutter_rust_bridge_codegen --version 1.53.0
    - Generate Dart files
    - Organize imports

--- lib/routes/home/widgets/status_text.dart
+++ lib/routes/home/widgets/status_text.dart
@@ -1,12 +1,8 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:c_breez/bloc/account/account_bloc.dart';
-import 'package:c_breez/bloc/account/account_state.dart';
 import 'package:c_breez/l10n/build_context_localizations.dart';
 import 'package:c_breez/utils/min_font_size.dart';
 import 'package:c_breez/widgets/loading_animated_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class StatusText extends StatelessWidget {
   final String? message;

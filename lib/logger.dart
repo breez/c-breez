6bf727e866cce209a688a2bf65bdd37b2fc556ec
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2022-12-23 13:58:32 +0300

    Migrate to file sized based logging with log rotation enabled

--- lib/logger.dart
+++ lib/logger.dart
@@ -1,6 +1,5 @@
 library breez.logger;
 
-import 'dart:async';
 import 'dart:io';
 
 import 'package:archive/archive_io.dart';
@@ -11,22 +10,12 @@ import 'package:share_extend/share_extend.dart';
 
 final _log = FimberLog("logger");
 
-Future<File> get _logFile async {
-  final appDir = await getApplicationDocumentsDirectory();
-  final logFile = File("${appDir.path}/c-breez.log");
-  if (!logFile.existsSync()) {
-    logFile.createSync();
-  }
-  return logFile;
-}
-
 void shareLog() async {
   final appDir = await getApplicationDocumentsDirectory();
-  final logFile = await _logFile;
   final encoder = ZipFileEncoder();
-  final zipFilePath = "${appDir.path}/c-breez.log.zip";
+  final zipFilePath = "${appDir.path}/c-breez.logs.zip";
   encoder.create(zipFilePath);
-  encoder.addFile(logFile);
+  encoder.addDirectory(Directory("${appDir.path}/logs/"));
   encoder.close();
   ShareExtend.share(zipFilePath, "file");
 }
@@ -47,25 +36,30 @@ class BreezLogger {
       ));
     }
 
-    _logFile.then((file) {
-      final tokens = [
-        CustomFormatTree.timeStampToken,
-        CustomFormatTree.levelToken,
-        CustomFormatTree.tagToken,
-        CustomFormatTree.messageToken,
-      ];
-      Fimber.plantTree(FimberFileTree(
-        file.path,
-        logLevels: logLevels,
-        logFormat: tokens.fold("", (p, e) => p == "" ? e : "$p :: $e"),
-      ));
-    });
-
-    FlutterError.onError = (FlutterErrorDetails details) async {
-      FlutterError.presentError(details);
-      final name = details.context?.name ?? "FlutterError";
-      final exception = details.exceptionAsString();
-      _log.e("$exception --$name", ex: details, stacktrace: details.stack);
-    };
+    getApplicationDocumentsDirectory().then(
+      (appDir) {
+        final tokens = [
+          CustomFormatTree.timeStampToken,
+          CustomFormatTree.levelToken,
+          CustomFormatTree.tagToken,
+          CustomFormatTree.messageToken,
+        ];
+        Fimber.plantTree(
+          SizeRollingFileTree(
+            DataSize(megabytes: 2),
+            filenamePrefix: "${appDir.path}/logs/c_breez.",
+            filenamePostfix: ".log",
+            logLevels: logLevels,
+            logFormat: tokens.fold("", (p, e) => p == "" ? e : "$p :: $e"),
+          ),
+        );
+        FlutterError.onError = (FlutterErrorDetails details) async {
+          FlutterError.presentError(details);
+          final name = details.context?.name ?? "FlutterError";
+          final exception = details.exceptionAsString();
+          _log.e("$exception --$name", ex: details, stacktrace: details.stack);
+        };
+      },
+    );
   }
 }

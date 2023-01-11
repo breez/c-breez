f0e8c61e50c5c24bc944ca80726c6180c2ca4199
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2023-01-11 23:21:19 +0300

    Launch links on external browser
    Differences to breezmobile implementation:
    - Added a loader before browser is opened
    - Added error handling & logging
    - Uses flutter_inappwebview plugin as flutter_web_browser was incompatible & outdated
    - OS based styling

---
+++ lib/utils/external_browser.dart
@@ -0,0 +1,48 @@
+import 'package:c_breez/widgets/loader.dart';
+import 'package:fimber/fimber.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+final _log = FimberLog("ExternalBrowser");
+
+Future<void> launchLinkOnExternalBrowser(
+  BuildContext context, {
+  required String linkAddress,
+}) async {
+  final texts = AppLocalizations.of(context)!;
+  final navigator = Navigator.of(context);
+  var loaderRoute = createLoaderRoute(context);
+  navigator.push(loaderRoute);
+  try {
+    if (await canLaunchUrlString(linkAddress)) {
+      final themeData = Theme.of(context);
+      await ChromeSafariBrowser().open(
+        url: WebUri(linkAddress),
+        settings: ChromeSafariBrowserSettings(
+          // Android
+          shareState: CustomTabsShareState.SHARE_STATE_ON,
+          showTitle: true,
+          // iOS
+          dismissButtonStyle: DismissButtonStyle.CLOSE,
+          barCollapsingEnabled: true,
+          // Styling
+          // Android
+          toolbarBackgroundColor: themeData.appBarTheme.backgroundColor,
+          navigationBarColor: themeData.bottomAppBarTheme.color,
+          // iOS 10.0+
+          preferredControlTintColor: Colors.white,
+          preferredBarTintColor: themeData.appBarTheme.backgroundColor,
+        ),
+      );
+    } else {
+      throw Exception("Can't launch url.");
+    }
+  } catch (error) {
+    _log.w("Can't launch url.", ex: error);
+    rethrow;
+  } finally {
+    navigator.removeRoute(loaderRoute);
+  }
+}

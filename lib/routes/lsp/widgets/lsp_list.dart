8ac329e3193a7937f6d43c176d43e85c676165ef
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2023-01-12 14:38:38 +0300

    Display a warning box when LSP list is empty
    - Added l10n values for link launch errors

--- lib/routes/lsp/widgets/lsp_list.dart
+++ lib/routes/lsp/widgets/lsp_list.dart
@@ -1,5 +1,8 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:breez_sdk/bridge_generated.dart';
 import 'package:c_breez/l10n/build_context_localizations.dart';
+import 'package:c_breez/utils/min_font_size.dart';
+import 'package:c_breez/widgets/warning_box.dart';
 import 'package:flutter/material.dart';
 
 class LspList extends StatefulWidget {
@@ -39,23 +42,38 @@ class _LspListState extends State<LspList> {
           Flexible(
             flex: 1,
             child: Padding(
-              padding: const EdgeInsets.all(48.0),
-              child: ListView.builder(
-                shrinkWrap: false,
-                itemCount: widget.lspList.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 0.0),
-                    title: Text(widget.lspList[index].name),
-                    selected:
-                        widget.selectedLsp?.id == widget.lspList[index].id,
-                    trailing: widget.selectedLsp?.id == widget.lspList[index].id
-                        ? const Icon(Icons.check)
-                        : null,
-                    onTap: () => widget.onSelected(widget.lspList[index]),
-                  );
-                },
-              ),
+              padding: EdgeInsets.all(widget.lspList.isEmpty ? 8.0 : 48.0),
+              child: widget.lspList.isEmpty
+                  ? Center(
+                      child: WarningBox(
+                        boxPadding: EdgeInsets.zero,
+                        contentPadding: const EdgeInsets.all(8),
+                        child: AutoSizeText(
+                          texts.account_page_activation_provider_unavailable,
+                          maxLines: 2,
+                          textAlign: TextAlign.center,
+                          minFontSize: MinFontSize(context).minFontSize,
+                        ),
+                      ),
+                    )
+                  : ListView.builder(
+                      shrinkWrap: false,
+                      itemCount: widget.lspList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          contentPadding:
+                              const EdgeInsets.symmetric(horizontal: 0.0),
+                          title: Text(widget.lspList[index].name),
+                          selected: widget.selectedLsp?.id ==
+                              widget.lspList[index].id,
+                          trailing:
+                              widget.selectedLsp?.id == widget.lspList[index].id
+                                  ? const Icon(Icons.check)
+                                  : null,
+                          onTap: () => widget.onSelected(widget.lspList[index]),
+                        );
+                      },
+                    ),
             ),
           ),
         ],

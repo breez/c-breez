5a5f6975787566b9039f0b8d84ebbaf9b8120d77
Ademar Alves de Oliveira <ademar111190@gmail.com>
2023-01-18 09:39:58 -0300

    Uses mobile scan instead of scan removing scan lib

--- lib/routes/qr_scan/widgets/qr_scan.dart
+++ lib/routes/qr_scan/widgets/qr_scan.dart
@@ -1,13 +1,13 @@
-import 'dart:io';
-
+import 'package:c_breez/l10n/build_context_localizations.dart';
+import 'package:c_breez/routes/qr_scan/scan_overlay.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_fimber/flutter_fimber.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
-import 'package:scan/scan.dart';
 
-import '../scan_overlay.dart';
+final _log = FimberLog("QRScan");
 
 class QRScan extends StatefulWidget {
   @override
@@ -18,106 +18,148 @@ class QRScan extends StatefulWidget {
 
 class QRScanState extends State<QRScan> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  var popped = false;
+  MobileScannerController cameraController = MobileScannerController(
+    facing: CameraFacing.back,
+    torchEnabled: false,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          buildScanner(context),
-          buildImagePicker(),
-          buildCancelButton(context),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0.0,
+            top: 0.0,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  flex: 5,
+                  child: MobileScanner(
+                    key: qrKey,
+                    allowDuplicates: false,
+                    controller: cameraController,
+                    onDetect: (barcode, args) {
+                      _log.i("Barcode detected: $barcode");
+                      if (popped || !mounted) return;
+                      final code = barcode.rawValue;
+                      if (code == null) {
+                        _log.w("Failed to scan QR code.");
+                      } else {
+                        popped = true;
+                        _log.i("Popping read QR code $code");
+                        Navigator.of(context).pop(code);
+                      }
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            right: 10,
+            top: 5,
+            child: ImagePickerButton(cameraController),
+          ),
+          Positioned(
+            bottom: 30.0,
+            right: 0,
+            left: 0,
+            child: defaultTargetPlatform == TargetPlatform.iOS ? const QRScanCancelButton() : const SizedBox(),
+          ),
           const ScanOverlay(),
         ],
       ),
     );
   }
+}
 
-  Positioned buildImagePicker() {
-    return Positioned(
-      right: 10,
-      top: 5,
-      child: IconButton(
-        padding: const EdgeInsets.fromLTRB(0, 32, 24, 0),
-        icon: SvgPicture.asset(
-          "src/icon/image.svg",
-          color: Colors.white,
-          width: 32,
-          height: 32,
+class ImagePickerButton extends StatelessWidget {
+  final MobileScannerController cameraController;
+
+  const ImagePickerButton(
+    this.cameraController, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final texts = context.texts();
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
+
+    return IconButton(
+      padding: const EdgeInsets.fromLTRB(0, 32, 24, 0),
+      icon: SvgPicture.asset(
+        "src/icon/image.svg",
+        color: Colors.white,
+        width: 32,
+        height: 32,
+      ),
+      onPressed: () async {
+        final picker = ImagePicker();
+        final pickedFile = await picker.pickImage(source: ImageSource.gallery).catchError((err) {
+          _log.w("Failed to pick image", ex: err);
+        });
+        final filePath = pickedFile?.path;
+        _log.i("Picked image: $filePath");
+        try {
+          final found = filePath != null && await cameraController.analyzeImage(filePath);
+          if (!found) {
+            _log.i("No QR code found in image");
+            scaffoldMessenger.showSnackBar(
+              SnackBar(
+                content: Text(texts.qr_scan_gallery_failed),
+              ),
+            );
+          } else {
+            _log.i("QR code found in image");
+          }
+        } catch (err) {
+          _log.w("Failed to analyze image", ex: err);
+        }
+      },
+    );
+  }
+}
+
+class QRScanCancelButton extends StatelessWidget {
+  const QRScanCancelButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final texts = context.texts();
+
+    return Center(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(
+            Radius.circular(12.0),
+          ),
+          border: Border.all(
+            color: Colors.white.withOpacity(0.8),
+          ),
         ),
-        onPressed: () async {
-          final picker = ImagePicker();
-          XFile? pickedFile = await picker
-              .pickImage(source: ImageSource.gallery)
-              .catchError((err) {});
-          final File file = File(pickedFile!.path);
-          try {
-            String? data = await Scan.parse(file.path);
-            if (!mounted) return;
-            Navigator.of(context).pop(data);
-          } catch (_) {}
-        },
-      ),
-    );
-  }
-
-  Positioned buildScanner(BuildContext context) {
-    return Positioned(
-      left: 0,
-      right: 0,
-      bottom: 0.0,
-      top: 0.0,
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 5,
-            child: MobileScanner(
-              key: qrKey,
-              allowDuplicates: false,
-              controller: MobileScannerController(
-                facing: CameraFacing.back,
-                torchEnabled: false,
-              ),
-              onDetect: (barcode, args) {
-                if (barcode.rawValue == null) {
-                  debugPrint('Failed to scan QR code.');
-                } else {
-                  final String code = barcode.rawValue!;
-                  Navigator.of(context).pop(code);
-                }
-              },
+        child: TextButton(
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.only(
+              right: 35,
+              left: 35,
             ),
-          )
-        ],
+          ),
+          onPressed: () => Navigator.of(context).pop(),
+          child: Text(
+            texts.qr_scan_action_cancel,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
-    );
-  }
-
-  Positioned buildCancelButton(BuildContext context) {
-    return Positioned(
-      bottom: 30.0,
-      right: 0,
-      left: 0,
-      child: defaultTargetPlatform == TargetPlatform.iOS
-          ? Center(
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(12.0)),
-                    border: Border.all(color: Colors.white.withOpacity(0.8))),
-                child: TextButton(
-                    style: TextButton.styleFrom(
-                      padding: const EdgeInsets.only(right: 35, left: 35),
-                    ),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: const Text(
-                      "CANCEL",
-                      style: TextStyle(color: Colors.white),
-                    )),
-              ),
-            )
-          : const SizedBox(),
     );
   }
 }

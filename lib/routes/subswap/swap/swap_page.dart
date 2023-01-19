1c4b675764d443b960586ce0262a9017cf4c0dc7
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2023-01-19 14:28:42 +0300

    Remove unconfirmedTxID related code

--- lib/routes/subswap/swap/swap_page.dart
+++ lib/routes/subswap/swap/swap_page.dart
@@ -22,35 +22,18 @@ class SwapPage extends StatefulWidget {
 class SwapPageState extends State<SwapPage> {
   final BreezBridge breezLib = ServiceInjector().breezLib;
   SwapInfo? swap;
-  String? unconfirmedTxID;
   String? bitcoinAddress;
   String? errorMessage;
 
   @override
   void didChangeDependencies() {
     getBTCAddress();
-    checkUnconfirmedTx();
     super.didChangeDependencies();
   }
 
-  // TODO: Get bitcoin address and listen to address status changes
   void getBTCAddress() async {
     try {
       swap = (await breezLib.receiveOnchain());
-      //errorMessage = "PLACEHOLDER_ERROR_STRING" ?? swap!.errorMessage;
-    } catch (e) {
-      errorMessage = extractExceptionMessage(e);
-    } finally {
-      setState(() {});
-    }
-  }
-
-  // TODO: check for unconfirmed Tx's
-  void checkUnconfirmedTx() async {
-    try {
-      // Get SwapLiveData list using subswapService and check for redeemable tx's
-      unconfirmedTxID = "PLACEHOLDER_TXID_STRING";
-      // TODO: Differentiate between error types
     } catch (e) {
       errorMessage = extractExceptionMessage(e);
     } finally {
@@ -61,8 +44,7 @@ class SwapPageState extends State<SwapPage> {
   @override
   Widget build(BuildContext context) {
     final texts = context.texts();
-    final bool isLoading =
-        unconfirmedTxID == null && swap == null && errorMessage == null;
+    final bool isLoading = swap == null && errorMessage == null;
 
     return Scaffold(
       appBar: AppBar(
@@ -75,27 +57,7 @@ class SwapPageState extends State<SwapPage> {
           : SingleChildScrollView(
               child: Column(
                 children: [
-                  // TODO: Move unconfirmed tx list to another page
-                  if (unconfirmedTxID != null) ...[
-/*                    Padding(
-                      padding: const EdgeInsets.only(left: 16.0, right: 16, top: 24),
-                      child: RichText(
-                        text: TextSpan(
-                          text:'You have transactions waiting for confirmation. Click ',
-                          children: [
-                            TextSpan(
-                              text: texts.invoice_btc_address_on_chain_here,
-                              style: const TextStyle(color: Colors.blue),
-                            ),
-                            const TextSpan(text: ' to see the list.'),
-                          ],
-                        ),
-                      ),
-                    ),
-                    UnconfirmedTxWidget(unconfirmedTxID: unconfirmedTxID!),*/
-                  ],
                   if (swap != null) ...[DepositWidget(swap!)],
-                  // TODO: Once unconfirmed tx list is moved to another page, make error message take priority over DepositWidget
                   if (errorMessage != null) ...[
                     Padding(
                       padding: const EdgeInsets.only(
@@ -115,7 +77,6 @@ class SwapPageState extends State<SwapPage> {
                   ? texts.invoice_btc_address_action_retry
                   : texts.invoice_btc_address_action_close,
               onPressed: () async {
-                // TODO: Make sure the action is appropriate with error message
                 if (errorMessage != null) {
                   getBTCAddress();
                 } else {

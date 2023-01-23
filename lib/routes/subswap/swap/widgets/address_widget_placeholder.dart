c487e865683805e5ef384172197124bb1f70bbd7
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2023-01-23 23:32:27 +0300

    Restyle QRCodePlaceholder to match Breezmobile UI

---
+++ lib/routes/subswap/swap/widgets/address_widget_placeholder.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AddressWidgetPlaceholder extends StatelessWidget {
+  const AddressWidgetPlaceholder({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Center(
+          child: Container(
+            height: 188,
+            width: 188,
+            margin: const EdgeInsets.only(top: 112.0),
+            padding: const EdgeInsets.all(8),
+            child: const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}

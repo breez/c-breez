722bb5906e5bab142d6d9e888e5dca2252363e06
Ademar Alves de Oliveira <ademar111190@gmail.com>
2023-01-26 15:47:19 -0300

    Add a profile bloc logic to fill missing properties on user profile

--- lib/bloc/user_profile/user_profile_bloc.dart
+++ lib/bloc/user_profile/user_profile_bloc.dart
@@ -1,33 +1,74 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:breez_translations/breez_translations_locales.dart';
+import 'package:c_breez/bloc/user_profile/default_profile_generator.dart';
 import 'package:c_breez/bloc/user_profile/user_profile_state.dart';
 import 'package:c_breez/models/user_profile.dart';
 import 'package:c_breez/services/breez_server.dart';
 import 'package:c_breez/services/notifications.dart';
+import 'package:fimber/fimber.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
 const PROFILE_DATA_FOLDER_PATH = "profile";
 
+final _log = FimberLog("UserProfileBloc");
+
 class UserProfileBloc extends Cubit<UserProfileState> with HydratedMixin {
   final BreezServer _breezServer;
   final Notifications _notifications;
 
-  UserProfileBloc(this._breezServer, this._notifications)
-      : super(UserProfileState.initial());
+  UserProfileBloc(
+    this._breezServer,
+    this._notifications,
+  ) : super(UserProfileState.initial()) {
+    stream.listen((profile) {
+      _log.v("State: ${profile.profileSettings.toJson()}");
+      final settings = profile.profileSettings;
+      if (settings.color == null || settings.animal == null) {
+        _log.v("Profile has not color or name, generating new random ones…");
+        final defaultProfile = generateDefaultProfile();
+        emit(profile.copyWith(
+          profileSettings: settings.copyWith(
+            color: settings.color ?? defaultProfile.color,
+            animal: settings.animal ?? defaultProfile.animal,
+          ),
+        ));
+      } else if (settings.name == null) {
+        _log.v("Profile has no name, generating new one…");
+        emit(profile.copyWith(
+          profileSettings: settings.copyWith(
+            name: DefaultProfile(
+              settings.color!,
+              settings.animal!,
+            ).buildName(getSystemLocale()),
+          ),
+        ));
+      } else if (!settings.registered) {
+        _log.v("Profile has not been registered, registering…");
+        registerForNotifications();
+      }
+    });
+    emit(state); // trigger hydration
+  }
 
   Future registerForNotifications() async {
+    _log.v("registerForNotifications");
     String? token = await _notifications.getToken();
     if (token != null) {
+      _log.v("Got token $token");
       var userID = await _breezServer.registerDevice(token, token);
       emit(state.copyWith(
           profileSettings: state.profileSettings.copyWith(
               token: token, userID: userID, registrationRequested: true)));
+    } else {
+      _log.w("Failed to get token");
     }
   }
 
   Future<String> uploadImage(List<int> bytes) async {
+    _log.v("uploadImage ${bytes.length}");
     try {
       // TODO upload image to server
       // return await _breezServer.uploadLogo(bytes);
@@ -45,6 +86,7 @@ class UserProfileBloc extends Cubit<UserProfileState> with HydratedMixin {
     bool? registrationRequested,
     bool? hideBalance,
   }) {
+    _log.v("updateProfile $name $color $animal $image $registrationRequested $hideBalance");
     var profile = state.profileSettings;
     profile = profile.copyWith(
         name: name ?? profile.name,

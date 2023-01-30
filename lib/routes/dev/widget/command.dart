3952877a1742206ae5845cc1ebcb545666ebac59
Ademar <ademar111190@gmail.com>
2023-01-30 13:09:08 -0300

    Broken widgets on dev page (#456)

---
+++ lib/routes/dev/widget/command.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Command extends StatelessWidget {
+  final String command;
+  final Function(String command) onTap;
+
+  const Command(
+    this.command,
+    this.onTap, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => onTap(command),
+      child: Container(
+        alignment: Alignment.centerLeft,
+        padding: const EdgeInsets.fromLTRB(24, 8, 24, 8),
+        child: Text(
+          command,
+          textAlign: TextAlign.left,
+        ),
+      ),
+    );
+  }
+}

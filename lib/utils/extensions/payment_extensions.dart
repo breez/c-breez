c3b4b7727e35a625804ff73aebf3ff8eed14952e
Ademar Alves de Oliveira <ademar111190@gmail.com>
2023-02-08 16:14:28 -0300

    Replicate description logic from breez mobile on items where data is available

--- lib/utils/extensions/payment_extensions.dart
+++ lib/utils/extensions/payment_extensions.dart
@@ -26,4 +26,33 @@ extension PaymentExtensions on Payment {
 
     return texts.wallet_dashboard_payment_item_no_title;
   }
+
+  String extractDescription(BreezTranslations texts) {
+    final description = this.description?.replaceAll("\n", " ").trim();
+
+    if (description != null && description.startsWith("Bitrefill")) {
+      return description.substring(9, description.length).trimLeft();
+    }
+
+    if (description != null && description.isNotEmpty) {
+      return extractPosMessage(description) ?? description;
+    }
+
+    final details = this.details;
+    if (details is PaymentDetails_ClosedChannel) {
+      final state = details.data.state;
+      switch (state) {
+        case ChannelState.PendingOpen:
+          return texts.payment_info_title_pending_opened_channel;
+        case ChannelState.Opened:
+          return texts.payment_info_title_opened_channel;
+        case ChannelState.PendingClose:
+          return texts.payment_info_title_pending_closed_channel;
+        case ChannelState.Closed:
+          return texts.payment_info_title_closed_channel;
+      }
+    }
+
+    return texts.wallet_dashboard_payment_item_no_title;
+  }
 }

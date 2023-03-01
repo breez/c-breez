5f96ce1caab4d85f387dafa8237553153cf348f7
Ademar Alves de Oliveira <ademar111190@gmail.com>
2023-03-01 14:24:08 -0300

    Add fallback option to preferences plus unit tests

--- lib/utils/preferences.dart
+++ lib/utils/preferences.dart
@@ -1,6 +1,7 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 const String _mempoolSpaceUrlKey = "mempool_space_url";
+const String _mempoolSpaceFallbackUrlKey = "mempool_space_url_fallback";
 
 class Preferences {
   Future<String?> getMempoolSpaceUrl() async {
@@ -16,5 +17,15 @@ class Preferences {
   Future<void> resetMempoolSpaceUrl() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove(_mempoolSpaceUrlKey);
+  }
+
+  Future<String?> getMempoolSpaceFallbackUrl() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_mempoolSpaceFallbackUrlKey);
+  }
+
+  Future<void> setMempoolSpaceFallbackUrl(String url) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_mempoolSpaceFallbackUrlKey, url);
   }
 }

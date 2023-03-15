2c2198602cb893069c69ae5e39d3330e05f7a3ca
Ademar Alves de Oliveira <ademar111190@gmail.com>
2023-03-15 09:46:47 -0300

    Add payment overrides preferences

--- test/mock/preferences_mock.dart
+++ test/mock/preferences_mock.dart
@@ -22,4 +22,43 @@ class PreferencesMock extends Mock implements Preferences {
     resetMempoolSpaceUrlCalled++;
     return Future<void>.value();
   }
+
+  bool paymentOptionsOverrideFeeEnabled = kDefaultOverrideFee;
+
+  @override
+  Future<bool> getPaymentOptionsOverrideFeeEnabled() => Future<bool>.value(paymentOptionsOverrideFeeEnabled);
+
+  int setPaymentOptionsOverrideFeeEnabledCalled = 0;
+
+  @override
+  Future<void> setPaymentOptionsOverrideFeeEnabled(bool enabled) {
+    setPaymentOptionsOverrideFeeEnabledCalled++;
+    return Future<void>.value();
+  }
+
+  int paymentOptionsBaseFee = kDefaultBaseFee;
+
+  @override
+  Future<int> getPaymentOptionsBaseFee() => Future<int>.value(paymentOptionsBaseFee);
+
+  int setPaymentOptionsBaseFeeCalled = 0;
+
+  @override
+  Future<void> setPaymentOptionsBaseFee(int fee) {
+    setPaymentOptionsBaseFeeCalled++;
+    return Future<void>.value();
+  }
+
+  double paymentOptionsProportionalFee = kDefaultProportionalFee;
+
+  @override
+  Future<double> getPaymentOptionsProportionalFee() => Future<double>.value(paymentOptionsProportionalFee);
+
+  int setPaymentOptionsProportionalFeeCalled = 0;
+
+  @override
+  Future<void> setPaymentOptionsProportionalFee(double fee) {
+    setPaymentOptionsProportionalFeeCalled++;
+    return Future<void>.value();
+  }
 }
--- test/utils/preferences_test.dart
+++ test/utils/preferences_test.dart
@@ -2,17 +2,74 @@ import 'package:c_breez/utils/preferences.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../unit_logger.dart';
+
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+  setUpLogger();
 
   setUp(() async {
     SharedPreferences.setMockInitialValues({});
   });
 
-  test('mempool space url', () async {
-    final preferences = Preferences();
-    const url = "a mempool space url";
-    await preferences.setMempoolSpaceUrl(url);
-    expect(await preferences.getMempoolSpaceUrl(), url);
+  group('mempool space url', () {
+    test('default', () async {
+      final preferences = Preferences();
+      expect(await preferences.getMempoolSpaceUrl(), null);
+    });
+
+    test('set', () async {
+      final preferences = Preferences();
+      const url = "a mempool space url";
+      await preferences.setMempoolSpaceUrl(url);
+      expect(await preferences.getMempoolSpaceUrl(), url);
+    });
+
+    test('reset', () async {
+      final preferences = Preferences();
+      const url = "a mempool space url";
+      await preferences.setMempoolSpaceUrl(url);
+      await preferences.resetMempoolSpaceUrl();
+      expect(await preferences.getMempoolSpaceUrl(), null);
+    });
+  });
+
+  group('payment options override fee', () {
+    test('default', () async {
+      final preferences = Preferences();
+      expect(await preferences.getPaymentOptionsOverrideFeeEnabled(), kDefaultOverrideFee);
+    });
+
+    test('set', () async {
+      final preferences = Preferences();
+      await preferences.setPaymentOptionsOverrideFeeEnabled(true);
+      expect(await preferences.getPaymentOptionsOverrideFeeEnabled(), true);
+    });
+  });
+
+  group('payment options base fee', () {
+    test('default', () async {
+      final preferences = Preferences();
+      expect(await preferences.getPaymentOptionsBaseFee(), kDefaultBaseFee);
+    });
+
+    test('set', () async {
+      final preferences = Preferences();
+      await preferences.setPaymentOptionsBaseFee(30);
+      expect(await preferences.getPaymentOptionsBaseFee(), 30);
+    });
+  });
+
+  group('payment options proportional fee', () {
+    test('default', () async {
+      final preferences = Preferences();
+      expect(await preferences.getPaymentOptionsProportionalFee(), kDefaultProportionalFee);
+    });
+
+    test('set', () async {
+      final preferences = Preferences();
+      await preferences.setPaymentOptionsProportionalFee(2.0);
+      expect(await preferences.getPaymentOptionsProportionalFee(), 2.0);
+    });
   });
 }

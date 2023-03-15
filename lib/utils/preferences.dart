f7b5cf3ecd91515f8eaf38a295fbc6ec45b16da8
Ademar Alves de Oliveira <ademar111190@gmail.com>
2023-03-15 09:46:47 -0300

    Add payment overrides preferences

--- lib/utils/preferences.dart
+++ lib/utils/preferences.dart
@@ -1,6 +1,16 @@
+import 'package:fimber/fimber.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-const String _mempoolSpaceUrlKey = "mempool_space_url";
+const kDefaultOverrideFee = false;
+const kDefaultBaseFee = 20;
+const kDefaultProportionalFee = 1.0;
+
+const _mempoolSpaceUrlKey = "mempool_space_url";
+const _kPaymentOptionOverrideFee = "payment_options_override_fee";
+const _kPaymentOptionBaseFee = "payment_options_base_fee";
+const _kPaymentOptionProportionalFee = "payment_options_proportional_fee";
+
+final _log = FimberLog("preferences");
 
 class Preferences {
   Future<String?> getMempoolSpaceUrl() async {
@@ -9,6 +19,7 @@ class Preferences {
   }
 
   Future<void> setMempoolSpaceUrl(String url) async {
+    _log.d("set mempool space url: $url");
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString(_mempoolSpaceUrlKey, url);
   }
@@ -16,5 +27,38 @@ class Preferences {
   Future<void> resetMempoolSpaceUrl() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove(_mempoolSpaceUrlKey);
+  }
+
+  Future<bool> getPaymentOptionsOverrideFeeEnabled() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(_kPaymentOptionOverrideFee) ?? kDefaultOverrideFee;
+  }
+
+  Future<void> setPaymentOptionsOverrideFeeEnabled(bool enabled) async {
+    _log.d("set payment options override fee enabled: $enabled");
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(_kPaymentOptionOverrideFee, enabled);
+  }
+
+  Future<int> getPaymentOptionsBaseFee() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(_kPaymentOptionBaseFee) ?? kDefaultBaseFee;
+  }
+
+  Future<void> setPaymentOptionsBaseFee(int fee) async {
+    _log.d("set payment options base fee: $fee");
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_kPaymentOptionBaseFee, fee);
+  }
+
+  Future<double> getPaymentOptionsProportionalFee() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getDouble(_kPaymentOptionProportionalFee) ?? kDefaultProportionalFee;
+  }
+
+  Future<void> setPaymentOptionsProportionalFee(double fee) async {
+    _log.d("set payment options proportional fee: $fee");
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble(_kPaymentOptionProportionalFee, fee);
   }
 }

1d664061f5f12c2e84d461f8b16a6109020304c4
Ademar Alves de Oliveira <ademar111190@gmail.com>
2023-03-20 15:10:25 -0300

    Extract validator and test it plus setting text field text out of build

--- lib/routes/payment_options/widget/base_fee_widget.dart
+++ lib/routes/payment_options/widget/base_fee_widget.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
+
 import 'package:breez_translations/breez_translations_locales.dart';
+import 'package:c_breez/bloc/payment_options/form_validator.dart';
 import 'package:c_breez/bloc/payment_options/payment_options_bloc.dart';
 import 'package:c_breez/bloc/payment_options/payment_options_state.dart';
 import 'package:fimber/fimber.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rxdart/rxdart.dart';
 
 final _log = FimberLog("BaseFeeWidget");
 
@@ -20,6 +25,20 @@ class BaseFeeWidget extends StatefulWidget {
 class _BaseFeeWidgetState extends State<BaseFeeWidget> {
   final _baseFeeController = TextEditingController();
 
+  StreamSubscription<PaymentOptionsState>? _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    SchedulerBinding.instance.addPostFrameCallback((_) => _initializeListeners());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _subscription?.cancel();
+  }
+
   @override
   Widget build(BuildContext context) {
     final texts = context.texts();
@@ -32,13 +51,6 @@ class _BaseFeeWidgetState extends State<BaseFeeWidget> {
             child: Form(
               child: BlocBuilder<PaymentOptionsBloc, PaymentOptionsState>(
                 builder: (context, state) {
-                  if (!state.saveEnabled) {
-                    final baseFee = state.baseFee.toString();
-                    if (_baseFeeController.text != baseFee) {
-                      _baseFeeController.text = baseFee;
-                    }
-                  }
-
                   return TextFormField(
                     enabled: state.overrideFeeEnabled,
                     keyboardType: const TextInputType.numberWithOptions(
@@ -52,30 +64,14 @@ class _BaseFeeWidgetState extends State<BaseFeeWidget> {
                       labelText: texts.payment_options_base_fee_label,
                       border: const UnderlineInputBorder(),
                     ),
-                    validator: (value) {
-                      if (value == null) {
-                        return texts.payment_options_base_fee_label;
-                      }
-                      if (value.isEmpty) {
-                        return texts.payment_options_base_fee_label;
-                      }
-                      try {
-                        final newBaseFee = int.parse(value);
-                        if (newBaseFee < 0) {
-                          return texts.payment_options_base_fee_label;
-                        }
-                      } catch (e) {
-                        return texts.payment_options_base_fee_label;
-                      }
-                      return null;
-                    },
+                    validator: baseFeeValidator,
                     onChanged: (value) {
                       _log.v("onChanged: $value");
                       int baseFee;
                       try {
                         baseFee = int.parse(value);
-                      } catch (e) {
-                        _log.w("Failed to parse $value as int", ex: e);
+                      } catch (_) {
+                        _log.v("Failed to parse $value as int");
                         return;
                       }
                       context.read<PaymentOptionsBloc>().setBaseFee(baseFee);
@@ -88,5 +84,20 @@ class _BaseFeeWidgetState extends State<BaseFeeWidget> {
         ),
       ],
     );
+  }
+
+  void _initializeListeners() {
+    final bloc = context.read<PaymentOptionsBloc>();
+    _subscription = bloc.stream.startWith(bloc.state).distinct().listen((state) {
+      if (!state.saveEnabled) {
+        final baseFee = state.baseFee.toString();
+        if (_baseFeeController.text != baseFee) {
+          _log.v("Setting base fee to $baseFee");
+          setState(() {
+            _baseFeeController.text = baseFee;
+          });
+        }
+      }
+    });
   }
 }

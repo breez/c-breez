8c119e9927570e891284b7913249528b1ae05913
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2023-10-04 20:56:34 +0300

    Beautify SwapErrorMessage

--- lib/routes/subswap/swap/swap_page.dart
+++ lib/routes/subswap/swap/swap_page.dart
@@ -8,6 +8,7 @@ import 'package:c_breez/routes/subswap/swap/widgets/deposit_widget.dart';
 import 'package:c_breez/routes/subswap/swap/widgets/inprogress_swap.dart';
 import 'package:c_breez/routes/subswap/swap/widgets/swap_error_message.dart';
 import 'package:c_breez/services/injector.dart';
+import 'package:c_breez/utils/exceptions.dart';
 import 'package:c_breez/widgets/back_button.dart' as back_button;
 import 'package:c_breez/widgets/single_button_bottom_bar.dart';
 import 'package:flutter/material.dart';
@@ -31,41 +32,41 @@ class SwapPageState extends State<SwapPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<SwapInProgressBloc, SwapInProgressState>(builder: (context, swapState) {
-      final SwapInProgressBloc swapInBloc = context.read();
-      final texts = context.texts();
-      return Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          leading: const back_button.BackButton(),
-          title: Text(texts.invoice_btc_address_title),
-        ),
-        body: swapState.isLoading
-            ? const AddressWidgetPlaceholder()
-            : SingleChildScrollView(
-                child: Column(
-                  children: [
-                    ...[
-                      if (swapState.unused != null)
-                        DepositWidget(swapState.unused!)
-                      else if (swapState.inProgress != null)
-                        SwapInprogress(swap: swapState.inProgress!)
+    final texts = context.texts();
+
+    return BlocBuilder<SwapInProgressBloc, SwapInProgressState>(
+      builder: (context, swapState) {
+        return Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            leading: const back_button.BackButton(),
+            title: Text(texts.invoice_btc_address_title),
+          ),
+          body: swapState.isLoading
+              ? const AddressWidgetPlaceholder()
+              : SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      ...[
+                        if (swapState.unused != null)
+                          DepositWidget(swapState.unused!)
+                        else if (swapState.inProgress != null)
+                          SwapInprogress(swap: swapState.inProgress!)
+                      ],
+                      if (swapState.error != null) ...[
+                        SwapErrorMessage(errorMessage: extractExceptionMessage(swapState.error!, texts)),
+                      ]
                     ],
-                    if (swapState.error != null) ...[
-                      SwapErrorMessage(errorMessage: swapState.error.toString()),
-                    ]
-                  ],
+                  ),
                 ),
-              ),
-        bottomNavigationBar: swapState.error != null
-            ? SingleButtonBottomBar(
-                text: texts.invoice_btc_address_action_retry,
-                onPressed: () async {
-                  swapInBloc.pollSwapAddress();
-                },
-              )
-            : const SizedBox(),
-      );
-    });
+          bottomNavigationBar: swapState.error != null
+              ? SingleButtonBottomBar(
+                  text: texts.invoice_btc_address_action_retry,
+                  onPressed: () => context.read<SwapInProgressBloc>().pollSwapAddress(),
+                )
+              : const SizedBox(),
+        );
+      },
+    );
   }
 }

b35c034d7c011ef714994bbfa06a85566178368f
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2023-10-04 20:56:55 +0300

    Pass ReverseSwapFeesRequest to fetchReverseSwapFees

--- lib/routes/withdraw/reverse_swap/in_progress/reverse_swaps_in_progress_page.dart
+++ lib/routes/withdraw/reverse_swap/in_progress/reverse_swaps_in_progress_page.dart
@@ -1,7 +1,5 @@
 import 'package:breez_sdk/bridge_generated.dart';
-import 'package:breez_translations/breez_translations_locales.dart';
 import 'package:c_breez/routes/withdraw/reverse_swap/in_progress/reverse_swap_in_progress.dart';
-import 'package:c_breez/widgets/back_button.dart' as back_button;
 import 'package:flutter/material.dart';
 
 class ReverseSwapsInProgressPage extends StatefulWidget {

88cc86c93e1fb126c8b5bf0d2b7b5ff8f9452585
ruben beck <ruben.beck@pm.me>
2023-10-16 17:07:30 +0200

    add funding and closing transaction to paymentMinutiae

--- lib/routes/home/widgets/payments_list/dialog/closed_channel_payment_details.dart
+++ lib/routes/home/widgets/payments_list/dialog/closed_channel_payment_details.dart
@@ -1,7 +1,9 @@
-import 'package:breez_sdk/bridge_generated.dart';
+import 'package:breez_sdk/bridge_generated.dart' as sdk;
 import 'package:breez_translations/breez_translations_locales.dart';
+import 'package:c_breez/config.dart';
 import 'package:c_breez/models/payment_minutiae.dart';
 import 'package:c_breez/routes/home/widgets/payments_list/dialog/tx_widget.dart';
+import 'package:c_breez/widgets/loader.dart';
 import 'package:flutter/material.dart';
 
 class ClosedChannelPaymentDetailsWidget extends StatelessWidget {
@@ -16,62 +18,61 @@ class ClosedChannelPaymentDetailsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
     final texts = context.texts();
+    return FutureBuilder<Config>(
+      future: Config.instance(),
+      builder: (BuildContext context, AsyncSnapshot<Config> snapshot) {
+        if (snapshot.hasData) {
+          final blockExplorer = snapshot.data!.defaultMempoolUrl;
+          if (paymentMinutiae.status == sdk.PaymentStatus.Complete) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    style: themeData.dialogTheme.contentTextStyle,
+                    text: texts.payment_details_dialog_closed_channel_local_wallet,
+                  ),
+                ),
+                if (paymentMinutiae.paymentType == sdk.PaymentType.ClosedChannel &&
+                    paymentMinutiae.closingTxid != null) ...[
+                  TxWidget(
+                    txURL: "$blockExplorer/tx/${paymentMinutiae.closingTxid!}",
+                    txID: paymentMinutiae.closingTxid!,
+                  ),
+                ],
+              ],
+            );
+          }
 
-    if (paymentMinutiae.status == PaymentStatus.Complete) {
-      return Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          RichText(
-            text: TextSpan(
-              style: themeData.dialogTheme.contentTextStyle,
-              text: texts.payment_details_dialog_closed_channel_local_wallet,
-            ),
-          ),
-          // TODO waiting for closeChannelTxUrl and closeChannelTx
-          const TxWidget(
-            txURL: "",
-            txID: "",
-          ),
-        ],
-      );
-    }
+          String estimation = texts.payment_details_dialog_closed_channel_transfer_no_estimation;
 
-    int lockHeight = 0; // TODO pendingExpirationHeight
-    double hoursToUnlock = 0.0; // TODO hoursToExpire
-
-    int roundedHoursToUnlock = hoursToUnlock.round();
-    String hoursToUnlockStr = roundedHoursToUnlock > 1
-        ? texts.payment_details_dialog_closed_channel_about_hours(
-            roundedHoursToUnlock.toString(),
-          )
-        : texts.payment_details_dialog_closed_channel_about_hour;
-    String estimation = lockHeight > 0 && hoursToUnlock > 0
-        ? texts.payment_details_dialog_closed_channel_transfer_estimation(
-            lockHeight,
-            hoursToUnlockStr,
-          )
-        : texts.payment_details_dialog_closed_channel_transfer_no_estimation;
-
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        RichText(
-          text: TextSpan(
-            style: themeData.dialogTheme.contentTextStyle,
-            text: estimation,
-          ),
-        ),
-        // TODO waiting for closeChannelTxUrl and closeChannelTx
-        const TxWidget(
-          txURL: "",
-          txID: "",
-        ),
-        // TODO waiting for remoteCloseChannelTxUrl and remoteCloseChannelTx
-        const TxWidget(
-          txURL: "",
-          txID: "",
-        ),
-      ],
+          return Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              RichText(
+                text: TextSpan(
+                  style: themeData.dialogTheme.contentTextStyle,
+                  text: estimation,
+                ),
+              ),
+              if (paymentMinutiae.fundingTxid != null) ...[
+                TxWidget(
+                  txURL: "$blockExplorer/tx/${paymentMinutiae.fundingTxid!}",
+                  txID: paymentMinutiae.fundingTxid!,
+                ),
+              ],
+              if (paymentMinutiae.closingTxid != null) ...[
+                TxWidget(
+                  txURL: "$blockExplorer/tx/${paymentMinutiae.closingTxid!}",
+                  txID: paymentMinutiae.closingTxid!,
+                ),
+              ]
+            ],
+          );
+        } else {
+          return const Loader();
+        }
+      },
     );
   }
 }

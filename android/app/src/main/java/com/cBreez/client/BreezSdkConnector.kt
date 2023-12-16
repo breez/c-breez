3d3a14541efe6cae396ad0839f5363b48d506cde
Roei Erez <roeierez@gmail.com>
2023-12-16 19:18:48 +0200

    simplify flow

--- android/app/src/main/java/com/cBreez/client/BreezSdkConnector.kt
+++ android/app/src/main/java/com/cBreez/client/BreezSdkConnector.kt
@@ -1,49 +1,57 @@
 package com.cBreez.client
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.util.Log
-import breez_sdk.BlockingBreezServices
-import breez_sdk.EnvironmentType
-import breez_sdk.GreenlightNodeConfig
-import breez_sdk.NodeConfig
-import breez_sdk.Payment
-import breez_sdk.connect
-import breez_sdk.defaultConfig
-import breez_sdk.mnemonicToSeed
+import breez_sdk.*
 import io.flutter.util.PathUtils
 
-class BreezSdkConnector(applicationContext: Context, paymentListener: (payment: Payment) -> Unit) {
-    internal var breezSDK: BlockingBreezServices? = null
-    private var _applicationContext: Context
-    private var _paymentListener: (payment: Payment) -> Unit
-
-    init {
-        _paymentListener = paymentListener
-        _applicationContext = applicationContext
-        connectSDK()
-    }
+class BreezSdkConnector() {
 
     companion object {
         private const val TAG = "BreezSdkConnector"
-    }
+        private var breezSDK: BlockingBreezServices? = null
 
-    private fun connectSDK(): BlockingBreezServices {
-        if (breezSDK == null) {
-            Log.i(TAG, "Connecting to Breez SDK")
-            // Select your seed, invite code and environment
-            // TODO(_): Read mnemonic from Keystore
-            val mnemonic = "<your-mnemonic>"
-            val seed = mnemonicToSeed(mnemonic)
-            val apiKey = _applicationContext.getString(R.string.breezApiKey)
-
-            // Create the default config
-            val glNodeConf = GreenlightNodeConfig(null, null)
-            val nodeConf = NodeConfig.Greenlight(glNodeConf)
-            val config = defaultConfig(EnvironmentType.PRODUCTION, apiKey, nodeConf)
-            config.workingDir = PathUtils.getDataDirectory(_applicationContext)
-            // Connect to the Breez SDK make it ready for use
-            breezSDK = connect(config, seed, SDKListener(_paymentListener))
+        internal fun connectSDK(applicationContext: Context): BlockingBreezServices {
+            synchronized(this) {
+                if (breezSDK == null) {
+                    Log.i(TAG, "Connecting to Breez SDK")
+                    // Select your seed, invite code and environment
+                    // TODO(_): Read mnemonic from Keystore
+                    val mnemonic = ""
+                    val seed = mnemonicToSeed(mnemonic)
+                    val apiKey = applicationContext.getString(R.string.breezApiKey)
+                    // Create the default config
+                    val glNodeConf = GreenlightNodeConfig(null, null)
+                    val nodeConf = NodeConfig.Greenlight(glNodeConf)
+                    val config = defaultConfig(EnvironmentType.PRODUCTION, apiKey, nodeConf)
+                    config.workingDir = PathUtils.getDataDirectory(applicationContext)
+                    // Connect to the Breez SDK make it ready for use
+                    breezSDK = connect(config, seed, SDKListener())
+                }
+                return breezSDK!!
+            }
         }
-        return breezSDK as BlockingBreezServices
     }
+//
+//    @Throws(java.lang.Exception::class)
+//    private fun readSecuredValued(preferences: SharedPreferences, key: String?): String? {
+//        val rawValue = preferences.getString(key, null)
+//        return decodeRawValue(rawValue)
+//    }
+//
+//    @Throws(java.lang.Exception::class)
+//    private fun decodeRawValue(value: String?): String? {
+//        if (value == null) {
+//            return null
+//        }
+//        var charset = Charset.forName("UTF-8");
+//        val data: ByteArray = Base64.decode(value, 0)
+//
+//        var d = RSACipher18Implementation(context = this.appContext)
+//
+//        val result: ByteArray = storageCipher.decrypt(data)
+//        return String(result, charset)
+//    }
+
 }
--- android/app/src/main/java/com/cBreez/client/BreezSdkWorker.kt
+++ android/app/src/main/java/com/cBreez/client/BreezSdkWorker.kt
@@ -11,17 +11,13 @@ import breez_sdk.BreezEvent
 import breez_sdk.EventListener
 import breez_sdk.Payment
 import breez_sdk.PaymentDetails
+import breez_sdk.PaymentStatus
 import com.cBreez.client.BreezNotificationService.Companion.createNotification
 import com.cBreez.client.Constants.NOTIFICATION_ID_PAYMENT_RECEIVED
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 
 
 // SDK events listener
-class SDKListener(private val paymentListener: (payment: Payment) -> Unit) : EventListener {
+class SDKListener() : EventListener {
     companion object {
         private const val TAG = "SDKListener"
     }
@@ -31,21 +27,14 @@ class SDKListener(private val paymentListener: (payment: Payment) -> Unit) : Eve
         if (e is BreezEvent.InvoicePaid) {
             val pD = e.details
             Log.v(TAG, "Received payment. Bolt11: ${pD.bolt11}\nPayment Hash:${pD.paymentHash}")
-            pD.payment?.let { paymentListener(it) }
         }
     }
 }
 
 open class BreezSdkWorker(appContext: Context, workerParams: WorkerParameters) :
     Worker(appContext, workerParams) {
-    companion object {
-        private const val TAG = "BreezSdkWorker"
-
-        private var mutex = Mutex()
-        internal var breezSDK: BlockingBreezServices? = null
-        internal var receivedPayments: ArrayList<String> = ArrayList()
-        internal var timerList: ArrayList<PaymentHashPoller> = ArrayList()
-    }
+    private val TAG = "BreezSdkWorker"
+    private var breezSDK: BlockingBreezServices? = null
 
     override fun getForegroundInfo(): ForegroundInfo {
         return ForegroundInfo(
@@ -61,117 +50,47 @@ open class BreezSdkWorker(appContext: Context, workerParams: WorkerParameters) :
 
     override fun doWork(): Result {
         try {
-            synchronized(this) {
-                startPaymentReceivedJob()
+            val paymentHash =
+                inputData.getString("PAYMENT_HASH") ?: throw Exception("Couldn't find payment hash")
+            val breezSDK = BreezSdkConnector.connectSDK(applicationContext)
+            while (true) {
+                val payment = breezSDK.paymentByHash(paymentHash)
+                if (payment?.status == PaymentStatus.COMPLETE) {
+                    this.onPaymentReceived(payment!!)
+                    return Result.success()
+                } else {
+                    Log.v(TAG,"Payment w/ paymentHash: $paymentHash not received yet.")
+                    Thread.sleep(1_000)
+                }
             }
+
         } catch (e: Exception) {
-            shutdown()
+            onPaymentFailed()
             return Result.failure()
         }
-        return Result.success()
     }
 
-    private fun startPaymentReceivedJob() {
-        val paymentHash =
-            inputData.getString("PAYMENT_HASH") ?: throw Exception("Couldn't find payment hash")
-        if (receivedPayments.contains(paymentHash)) {
-            Log.i(TAG, "There's already a running job for this payment($paymentHash).")
-            return
-        }
-        receivedPayments.add(paymentHash)
-        if (breezSDK == null) {
-            try {
-                val paymentListener: (payment: Payment) -> Unit = { payment ->
-                    CoroutineScope(Dispatchers.Default).launch {
-                        mutex.withLock {
-                            onPaymentReceived(payment)
-                        }
-                    }
-                }
-                breezSDK = BreezSdkConnector(applicationContext, paymentListener).breezSDK
-            } catch (e: Exception) {
-                Log.e(TAG, "Failure during connecting to Breez SDK. Shutting down.", e)
-                shutdown()
-            }
-        }
-        // TODO: Add timeout to all payment has poller timers as they seem to run for a very long time, at least 1h+
-        startPaymentHashPollerTimer(paymentHash)
+    override fun onStopped() {
+        this.onPaymentFailed()
     }
 
-    private fun shutdown() {
+    private fun onPaymentFailed() {
         // Display a notification for each remaining payment in list
-        if (receivedPayments.size != 0) {
-            val contentText = applicationContext.resources.getQuantityString(
-                R.plurals.failed_notification_message, receivedPayments.size
-            )
-            createNotification(
-                applicationContext,
-                contentText,
-                NOTIFICATION_ID_PAYMENT_RECEIVED,
-            )
-        }
-        // Empty the received payments list and cancel payment hash poller timer
-        breezSDK = null
-        receivedPayments.clear()
-        timerList.forEach { timer ->
-            timer.cancel()
-        }
-    }
-
-    private fun startPaymentHashPollerTimer(paymentHash: String) {
-        // Todo: Remove this condition as there shouldn't be an existing job for the same payment hash
-        if (!containsTimer(paymentHash)) {
-            // Create a separate timer for each notification
-            CoroutineScope(Dispatchers.Main).launch {
-                // Todo: No need to lock thread for the same reason above
-                mutex.withLock {
-                    // Poll payment hash every second for a minute in case it's BreezEvent is missed
-                    var paymentHashPollerTimer = PaymentHashPoller(paymentHash, { payment ->
-                        onPaymentReceived(payment)
-                    }, {
-                        stopPaymentHashPollerTimer(paymentHash)
-                    })
-                    paymentHashPollerTimer.start()
-                    timerList.add(paymentHashPollerTimer)
-                }
-            }
-        }
-    }
-
-    private fun stopPaymentHashPollerTimer(paymentHash: String) {
-        Log.v(TAG, "Cancel PaymentHashPoller for: $paymentHash.")
-        receivedPayments.remove(paymentHash)
-        receivedPayments.removeIf { it == paymentHash }
-        timerList.removeIf { it.paymentHash == paymentHash }
+        var contentText = "Failed to receive payment"
+        createNotification(
+            applicationContext,
+            contentText,
+            NOTIFICATION_ID_PAYMENT_RECEIVED,
+        )
     }
 
     private fun onPaymentReceived(payment: Payment) {
-        val paymentDetails = payment.details
-        if (paymentDetails is PaymentDetails.Ln) {
-            val data = paymentDetails.guard { return }
-            if (receivedPayments.contains(data.data.paymentHash)) {
-                createNotification(
-                    applicationContext, "Received ${payment.amountMsat / 1000u} sats"
-                )
-            }
-            receivedPayments.removeIf { it == data.data.paymentHash }
-        }
+        createNotification(
+            applicationContext, "Received ${payment.amountMsat / 1000u} sats"
+        )
     }
 
     private inline fun <T> T.guard(block: T.() -> Unit): T {
         if (this == null) block(); return this
-    }
-
-    open fun containsTimer(paymentHash: String): Boolean {
-        for (timer in timerList) {
-            if (timer.paymentHash == paymentHash) {
-                Log.i(
-                    TAG,
-                    "There's already a running payment hash poller for this payment($paymentHash)."
-                )
-                return true
-            }
-        }
-        return false
     }
 }

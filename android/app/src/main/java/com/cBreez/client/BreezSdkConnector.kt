311a03e9a7ebda67127a3c868569620f2f5a0935
Erdem Yerebasmaz <4012752+erdemyerebasmaz@users.noreply.github.com>
2024-01-04 14:45:35 +0300

    Revert "Disconnect Breez SDK on shutdown"
    This reverts commit fe5a96ba63259b3ef6a741e0ecee7a6ca6d4b549.
    
    There's a race condition where if the App is opened through clicking the notification, disconnect happens while the app is still opening the app crashes.

--- android/app/src/main/java/com/cBreez/client/BreezSdkConnector.kt
+++ android/app/src/main/java/com/cBreez/client/BreezSdkConnector.kt
@@ -68,15 +68,5 @@ class BreezSdkConnector {
             val result: ByteArray = storageCipher.decrypt(data)
             return String(result, charset)
         }
-
-        internal fun disconnectSDK() {
-            if(breezSDK != null){
-                // Connect to the Breez SDK make it ready for use
-                Logger.tag(TAG).debug { "Disconnecting Breez SDK" }
-                breezSDK!!.disconnect()
-                Logger.tag(TAG).debug { "Disconnected from Breez SDK" }
-                breezSDK = null
-            }
-        }
     }
 }

7ffa151b7c49abbb4df5a909348afc2784c28b6c
Erdem Yerebasmaz <4012752+erdemyerebasmaz@users.noreply.github.com>
2024-01-29 17:09:07 +0300

    Get BreezSDKPlugin from registry

--- android/app/src/main/kotlin/com/cBreez/client/MainActivity.kt
+++ android/app/src/main/kotlin/com/cBreez/client/MainActivity.kt
@@ -4,16 +4,14 @@ import androidx.lifecycle.lifecycleScope
 import com.breez.breez_sdk.BreezSDKPlugin
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 import org.tinylog.kotlin.Logger
 
 class MainActivity : FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         val pluginRegistry = flutterEngine.plugins
-        val breezSdkPlugin = BreezSDKPlugin()
-        val nodeLogStream = breezSdkPlugin.setNodeLogStream()
-        nodeLogStream.subscribe(lifecycleScope) { l ->
+        val breezSdkPlugin = pluginRegistry.get(BreezSDKPlugin::class.java) as BreezSDKPlugin
+        breezSdkPlugin.setNodeLogStream().subscribe(lifecycleScope) { l ->
             when (l.level) {
                 "ERROR" -> Logger.tag(TAG).error { l.line }
                 "WARN" -> Logger.tag(TAG).warn { l.line }
@@ -21,8 +19,6 @@ class MainActivity : FlutterActivity() {
                 //"DEBUG" -> Logger.tag(TAG).debug { l.line }
             }
         }
-        pluginRegistry.add(breezSdkPlugin)
-        //GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
 
     companion object {

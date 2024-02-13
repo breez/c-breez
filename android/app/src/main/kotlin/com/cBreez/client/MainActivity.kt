acfd3c0162b7b5fb815d0a8e720688cad660950f
Erdem Yerebasmaz <4012752+erdemyerebasmaz@users.noreply.github.com>
2024-02-14 01:29:46 +0300

    Log DEBUG level SDK logs

--- android/app/src/main/kotlin/com/cBreez/client/MainActivity.kt
+++ android/app/src/main/kotlin/com/cBreez/client/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.cBreez.client
 
 import androidx.lifecycle.lifecycleScope
+import breez_sdk.LogEntry
 import com.breez.breez_sdk.BreezSDKPlugin
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
@@ -11,12 +12,14 @@ class MainActivity : FlutterActivity() {
         super.configureFlutterEngine(flutterEngine)
         val pluginRegistry = flutterEngine.plugins
         val breezSdkPlugin = pluginRegistry.get(BreezSDKPlugin::class.java) as BreezSDKPlugin
-        breezSdkPlugin.setNodeLogStream().subscribe(lifecycleScope) { l ->
+        val sdkLogListener = breezSdkPlugin.setNodeLogStream()
+        sdkLogListener.subscribe(lifecycleScope) { l: LogEntry ->
             when (l.level) {
                 "ERROR" -> Logger.tag(TAG).error { l.line }
                 "WARN" -> Logger.tag(TAG).warn { l.line }
                 "INFO" -> Logger.tag(TAG).info { l.line }
-                //"DEBUG" -> Logger.tag(TAG).debug { l.line }
+                "DEBUG" -> Logger.tag(TAG).debug { l.line }
+                //"TRACE" -> Logger.tag(TAG).trace { l.line }
             }
         }
     }

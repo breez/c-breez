98d9941c9491c2d963ff085e19e0889f37a22f43
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-03-06 19:57:50 +0300

    Configure tinylog logger on app side

--- android/app/src/main/kotlin/com/cBreez/client/BreezFcmService.kt
+++ android/app/src/main/kotlin/com/cBreez/client/BreezFcmService.kt
@@ -9,15 +9,13 @@ import android.os.Process
 import android.os.SystemClock
 import androidx.core.content.ContextCompat
 import breez_sdk_notification.Constants
-import breez_sdk_notification.LogHelper.Companion.configureLogger
 import breez_sdk_notification.Message
 import breez_sdk_notification.MessagingService
+import com.cBreez.client.BreezLogger.Companion.configureLogger
 import com.google.android.gms.common.util.PlatformVersion.isAtLeastLollipop
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import io.flutter.util.PathUtils
 import org.tinylog.kotlin.Logger
-import java.io.File
 
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
 class BreezFcmService : MessagingService, FirebaseMessagingService() {
@@ -27,7 +25,7 @@ class BreezFcmService : MessagingService, FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
-        configureLogger(getLoggingDir())
+        configureLogger(applicationContext)
         Logger.tag(TAG).debug { "FCM message received!" }
 
         if (remoteMessage.priority == RemoteMessage.PRIORITY_HIGH) {
@@ -37,15 +35,6 @@ class BreezFcmService : MessagingService, FirebaseMessagingService() {
                 ?.also { message -> startServiceIfNeeded(applicationContext, message) }
         } else {
             Logger.tag(TAG).debug { "Ignoring FCM message" }
-        }
-    }
-
-    private fun getLoggingDir(): File {
-        /** Get `/logs` folder from Flutter app data directory */
-        return File(PathUtils.getDataDirectory(applicationContext), "/logs").apply {
-            /** Create a new directory denoted by the pathname and also
-             *  all the non existent parent directories of the pathname */
-            mkdirs()
         }
     }
 
---
+++ android/app/src/main/kotlin/com/cBreez/client/BreezLogger.kt
@@ -0,0 +1,36 @@
+package com.cBreez.client
+
+import android.content.Context
+import io.flutter.util.PathUtils
+import org.tinylog.kotlin.Logger
+import java.io.File
+
+class BreezLogger {
+    companion object {
+        private const val TAG = "BreezLogger"
+
+        private var isInit: Boolean? = null
+
+        internal fun configureLogger(applicationContext: Context): Boolean? {
+            synchronized(this) {
+                /** Get `/logs` folder from Flutter app data directory */
+                val loggingDir =
+                    File(PathUtils.getDataDirectory(applicationContext), "/logs").apply {
+                        /** Create a new directory denoted by the pathname and also
+                         *  all the non existent parent directories of the pathname */
+                        mkdirs()
+                    }
+
+                System.setProperty("tinylog.directory", loggingDir.absolutePath)
+                System.setProperty("tinylog.timestamp", System.currentTimeMillis().toString())
+
+                if (isInit == false) {
+                    Logger.tag(TAG).debug { "Starting ${BuildConfig.APPLICATION_ID}..." }
+                    Logger.tag(TAG).debug { "Logs directory: '$loggingDir'" }
+                    isInit = true
+                }
+                return isInit
+            }
+        }
+    }
+}

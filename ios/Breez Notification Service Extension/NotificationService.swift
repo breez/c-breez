1447f5d18123a00f17d79c682be9e080b7b68988
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-03-06 21:08:15 +0300

    Set ServiceLogger to use SdkLogListener that utilizes XCGLogger on init

--- ios/Breez Notification Service Extension/NotificationService.swift
+++ ios/Breez Notification Service Extension/NotificationService.swift
@@ -4,24 +4,33 @@ import XCGLogger
 let accessGroup = "group.F7R2LZH3W5.com.cBreez.client"
 
 class NotificationService: SDKNotificationService {
+    fileprivate let TAG = "NotificationService"
     private let accountMnemonic: String = "account_mnemonic"
     private let accountApiKey: String = "account_api_key"
     
-    override init() {
+    private let xcgLogger: XCGLogger
+    
+    init() {
         let logsDir = FileManager
             .default.containerURL(forSecurityApplicationGroupIdentifier: accessGroup)!.appendingPathComponent("logs")
         let extensionLogFile = logsDir.appendingPathComponent("\(Date().timeIntervalSince1970).ios-extension.log")
         
-        super.init()
-        self.logger.setup(level: .debug, showThreadName: true, showLevel: true, showFileNames: true, showLineNumbers: true, writeToFile: extensionLogFile.path)
+        xcgLogger = {
+            let log = XCGLogger.default
+            log.setup(level: .debug, showThreadName: true, showLevel: true, showFileNames: true, showLineNumbers: true, writeToFile: extensionLogFile.path)
+            return log
+            
+        }()
+        let serviceLogger = ServiceLogger(logStream: SdkLogListener(logger: xcgLogger))
+        self.logger = serviceLogger
     }
     
     override func getConnectRequest() -> ConnectRequest? {
         guard let apiKey = KeychainHelper.shared.getFlutterString(accessGroup: accessGroup, key: accountApiKey) else {
-            logger.error("API key not found")
+            self.logger.log(tag: TAG, line: "API key not found", level: "ERROR")
             return nil
         }
-        logger.verbose("API_KEY: \(apiKey)")
+        self.logger.log(tag: TAG, line: "API_KEY: \(apiKey)", level: "TRACE")
         var config = defaultConfig(envType: EnvironmentType.production, apiKey: apiKey,
                                    nodeConfig: NodeConfig.greenlight(
                                     config: GreenlightNodeConfig(partnerCredentials: nil, inviteCode: nil)))
@@ -31,11 +40,11 @@ class NotificationService: SDKNotificationService {
         
         // Construct the seed
         guard let mnemonic = KeychainHelper.shared.getFlutterString(accessGroup: accessGroup, key: accountMnemonic) else {
-            logger.error("Mnemonic not found")
+            self.logger.log(tag: TAG, line: "Mnemonic not found", level: "ERROR")
             return nil
         }
         guard let seed = try? mnemonicToSeed(phrase: mnemonic) else {
-            logger.error("Invalid seed")
+            self.logger.log(tag: TAG, line: "Invalid seed", level: "ERROR")
             return nil
         }
         return ConnectRequest(config: config, seed: seed)

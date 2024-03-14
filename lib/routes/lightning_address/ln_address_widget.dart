046db35c14610b121c58b92123db99c2ddd880bb
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-03-14 23:18:41 +0300

    Calculate maximum receivable amount within fee limit

--- lib/routes/lightning_address/ln_address_widget.dart
+++ lib/routes/lightning_address/ln_address_widget.dart
@@ -1,7 +1,9 @@
 import 'package:breez_translations/breez_translations_locales.dart';
+import 'package:c_breez/bloc/payment_options/payment_options_bloc.dart';
 import 'package:c_breez/widgets/address_widget.dart';
 import 'package:c_breez/widgets/receivable_btc_box.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LnAddressWidget extends StatefulWidget {
   final String lnurlPayUrl;
@@ -15,6 +17,7 @@ class LnAddressWidget extends StatefulWidget {
 class _LnAddressWidgetState extends State<LnAddressWidget> {
   @override
   Widget build(BuildContext context) {
+    final channelFeeLimitMsat = context.read<PaymentOptionsBloc>().state.channelFeeLimitMsat;
     final texts = context.texts();
 
     return Column(
@@ -26,7 +29,10 @@ class _LnAddressWidgetState extends State<LnAddressWidget> {
           type: AddressWidgetType.lnurl,
           footer: widget.lnurlPayUrl,
         ),
-        const ReceivableBTCBox(),
+        Padding(
+          padding: const EdgeInsets.only(left: 24.0),
+          child: ReceivableBTCBox(channelFeeLimitSat: channelFeeLimitMsat ~/ 1000),
+        ),
       ],
     );
   }

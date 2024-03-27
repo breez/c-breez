19399cec81411d3012b96117a9919aa0df3cc381
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-03-27 19:22:26 +0300

    Revise swap in progress bloc & state
    Do not stop polling even if swap in progress is waiting for confirmation.

--- lib/bloc/swap_in_progress/swap_in_progress_state.dart
+++ lib/bloc/swap_in_progress/swap_in_progress_state.dart
@@ -6,5 +6,21 @@ class SwapInProgressState {
   final bool isLoading;
   final String? error;
 
-  SwapInProgressState(this.inProgress, this.unused, {this.isLoading = false, this.error});
+  SwapInProgressState({this.inProgress, this.unused, this.isLoading = false, this.error});
+
+  SwapInProgressState.initial() : this();
+
+  SwapInProgressState copyWith({
+    SwapInfo? inProgress,
+    SwapInfo? unused,
+    bool? isLoading,
+    String? error,
+  }) {
+    return SwapInProgressState(
+      inProgress: inProgress ?? this.inProgress,
+      unused: unused ?? this.unused,
+      isLoading: isLoading ?? this.isLoading,
+      error: error ?? this.error,
+    );
+  }
 }
